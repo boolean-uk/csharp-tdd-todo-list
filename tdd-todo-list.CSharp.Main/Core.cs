8b1dc531c39af97b0ef2689ae962f79626cbb95e
Nikita Pieters <nikita_pieters@hotmail.com>
2023-05-09 17:52:58 +0200

    done with halve of the standard requirements

--- tdd-todo-list.CSharp.Main/Core.cs
+++ tdd-todo-list.CSharp.Main/Core.cs
@@ -8,5 +8,59 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoList
     {
+        private Dictionary<string, bool> todo = new Dictionary<string, bool>();
+        Dictionary<string, bool> completedList = new Dictionary<string, bool>();
+        Dictionary<string, bool> incompletedList = new Dictionary<string, bool>();
+
+
+
+        public void AddTask(string task, bool status)
+        {
+            todo.Add(task, status);
+        }
+
+        public string GetTasks()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string task in todo.Keys) 
+            {
+                sb.Append($"{task}; ");
+            }
+            return sb.ToString();
+        }
+
+        public void UpdateTaskStatus(string task, bool status)
+        {
+            todo[task] = status;
+        }
+
+        public void CompletedTasks()
+        {
+            foreach(string task in todo.Keys)
+            {
+                if (todo[task] == true)
+                {
+                    completedList.Add(task, todo[task]);
+                }
+
+            }
+        }
+
+        public void IncompletedTasks()
+        {
+            foreach (string task in todo.Keys)
+            {
+                if (todo[task] == false)
+                {
+                    incompletedList.Add(task, todo[task]);
+                }
+
+            }
+        }
+
+        public Dictionary<string, bool> TaskList { get { return todo; } }
+        public Dictionary<string, bool> CompletedList { get { return completedList; } }
+        public Dictionary<string, bool> IncompletedList { get { return incompletedList; } }
+
     }
 }
--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -3,12 +3,83 @@ using NUnit.Framework;
 
 namespace tdd_todo_list.CSharp.Test
 {
+    [TestFixture]
     public class CoreTests
     {
         private TodoList _core;
         public CoreTests()
         {
             _core = new TodoList();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestAddTask()
+        {
+            TodoList _todoList = new TodoList();
+            _todoList.AddTask("Do laundry", false);
+            Assert.IsTrue(_todoList.TaskList.ContainsKey("Do laundry"));
+        }
+
+        [Test]
+        public void TestSeeTaskList()
+        {
+            TodoList _todoList = new TodoList();
+            string task1 = "Do laundry";
+            string task2 = "Make diner";
+            string task3 = "Have a meeting";
+            _todoList.AddTask(task1, false);
+            _todoList.AddTask(task2, false);
+            _todoList.AddTask(task3, false);
+            Assert.IsTrue(_todoList.TaskList.Count == 3);
+            string result = _todoList.GetTasks();
+            Assert.AreEqual(result, $"{task1}; {task2}; {task3}; ");
+        }
+
+        [Test]
+        public void TestUpdateStatus()
+        {
+            TodoList _todoList = new TodoList();
+            _todoList.AddTask("Do laundry", false);
+            _todoList.UpdateTaskStatus("Do laundry", true);
+            Assert.IsTrue(_todoList.TaskList["Do laundry"] == true);
+        }
+
+        [Test]
+        public void TestCompletedTasks()
+        {
+            TodoList _todoList = new TodoList();
+            string task1 = "Do laundry";
+            string task2 = "Make diner";
+            string task3 = "Have a meeting";
+            _todoList.AddTask(task1, false);
+            _todoList.AddTask(task2, false);
+            _todoList.AddTask(task3, false);
+            _todoList.UpdateTaskStatus(task2, true);
+            _todoList.UpdateTaskStatus(task1, true);
+            _todoList.CompletedTasks();
+            Assert.IsTrue(_todoList.CompletedList.ContainsKey(task2));
+            Assert.IsTrue(_todoList.CompletedList.ContainsKey(task1));
+        }
+        [Test]
+        public void TestIncompletedTasks()
+        {
+            TodoList _todoList = new TodoList();
+            string task1 = "Do laundry";
+            string task2 = "Make diner";
+            string task3 = "Have a meeting";
+            _todoList.AddTask(task1, false);
+            _todoList.AddTask(task2, false);
+            _todoList.AddTask(task3, false);
+            _todoList.UpdateTaskStatus(task2, true);
+            _todoList.IncompletedTasks();
+            Assert.IsTrue(_todoList.IncompletedList.ContainsKey(task3));
+            Assert.IsTrue(_todoList.IncompletedList.ContainsKey(task1));
         }
     }
 }
