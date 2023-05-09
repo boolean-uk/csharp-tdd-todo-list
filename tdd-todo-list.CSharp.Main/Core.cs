635d174fb4a52c7b148ebf90318f835eeb645561
Valentina <valentinagalyam@gmail.com>
2023-05-09 11:33:29 +0200

    add tasks and see all the tasks

--- tdd-todo-list.CSharp.Main/Core.cs
+++ tdd-todo-list.CSharp.Main/Core.cs
@@ -8,5 +8,21 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoList
     {
+        private List<string> tasks = new List<string>();
+
+        public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+        public int Count { get { return tasks.Count; } }
+
+        public string GetTask(int index) { return tasks[index]; }
+
+        public List<string> GetAllTasks() 
+        {
+            return tasks;
+        }
+
     }
+
 }
--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -5,10 +5,48 @@ namespace tdd_todo_list.CSharp.Test
 {
     public class CoreTests
     {
-        private TodoList _core;
-        public CoreTests()
+        // I want to add tasks to my todo list.
+        [Test]
+        public void TestTaskToList()
         {
-            _core = new TodoList();
+            //Arrange
+
+             var todoList = new TodoList();
+
+            // Act
+             todoList.AddTask("Task1");
+
+
+            // Assert
+            Assert.AreEqual(1, todoList.Count);
+            Assert.AreEqual("Task1", todoList.GetTask(0));
+
         }
+
+        // I want to see all the tasks in my todo list.
+
+        [Test]
+        public void TestAllTheTasks()
+        {
+            // Arrange
+
+            var todoList = new TodoList();
+            todoList.AddTask("Task1");
+            todoList.AddTask("Task2");
+
+            // Act
+
+            var tasks = todoList.GetAllTasks();
+
+            // Assert
+
+            Assert.AreEqual(new List<string> { "Task1", "Task2" }, tasks);
+
+        }
+
+
+        //  I want to change the status of a task between incomplete and complete.
+
+
     }
 }
