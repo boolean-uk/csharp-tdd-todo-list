14898179736a229335bc49dca65159a42fbcf2c3
Klara Andersson <klara.andersson@se.experis.com>
2024-01-12 16:16:38 +0100

    Core and extension done, with domain models

--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -1,12 +1,100 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace tdd_todo_list.CSharp.Main
 {
+    public struct Tasks
+    {
+        public string name;
+        public bool completed;
+        public int ID;
+        public int date;
+        public int time;
+    }
+
     public class TodoListExtension
     {
+        private List<Tasks> tasks = new List<Tasks>();
+
+        public void AddTask(string taskName, bool completed, int ID, int date, int time)
+        {
+            Tasks temp = new Tasks();
+            temp.name = taskName;
+            temp.completed = completed;
+            temp.ID = ID;
+            temp.date = date;
+            temp.time = time;
+
+            tasks.Add(temp);
+        }
+
+        public string GetName(int ID)
+        {
+            string message = "Do not exist";
+
+            for (int i = 0; i < tasks.Count(); i++)
+            {
+                if (tasks[i].ID == ID)
+                    message = tasks[i].name;
+            }
+
+            return message;
+        }
+
+        public bool NewName(int ID, string newName)
+        {
+            bool changedName = false;
+
+            for (int i = 0; i < tasks.Count(); i++)
+            {
+                if (tasks[i].ID == ID)
+                {
+                    tasks[i].name.Replace(tasks[i].name, newName);
+                    changedName = true;
+                }
+                    
+            }
+
+            return changedName;
+        }
+
+        public bool ChangeStatus(int ID)
+        {
+            bool changedStatus = false;
+
+            for (int i = 0; i < tasks.Count(); i++)
+            {
+                if (tasks[i].ID == ID)
+                {
+                    tasks[i].completed.ToString().Replace(tasks[i].completed.ToString(), (!tasks[i].completed).ToString());
+                    changedStatus = true;
+                }
+
+            }
+
+            return changedStatus;
+        }
+
+        public string GetDayTime(int ID)
+        {
+            string message = "Do not exist";
+
+            for (int i = 0; i < tasks.Count(); i++)
+            {
+                if (tasks[i].ID == ID)
+                {
+                    message = tasks[i].date.ToString();
+                    message += ", ";
+                    message += tasks[i].time.ToString();
+                }
+            }
+
+            return message;
+        }
     }
 }
--- tdd-todo-list.CSharp.Main/ToDoList.cs
+++ tdd-todo-list.CSharp.Main/ToDoList.cs
@@ -8,5 +8,126 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoList
     {
+        public Dictionary<string, int> todo;
+
+        public TodoList()
+        {
+            todo = new Dictionary<string, int>();
+        }
+
+        public void AddTasks(string taskName, int status)
+        {
+            todo.Add(taskName, status);
+        }
+
+        public string PrintList()
+        {
+            string tasks = "";
+
+            for (int i = 0; i < todo.Count(); i++)
+            {
+                tasks += todo.ElementAt(i).Key;
+                tasks += " ";
+            }
+
+            return tasks;
+        }
+
+        public bool CompleteTask(string taskName, int status)
+        {
+            if (status != 0 && status != 1)
+                return false;
+
+            if (!todo.ContainsKey(taskName))
+                return false;
+
+            todo[taskName] = status;
+            return true;
+        }
+
+        public string CompleteTasks()
+        {
+            string tasks = "";
+
+            for (int i = 0; i < todo.Count(); i++)
+            {
+                if (todo.ElementAt(i).Value == 1)
+                {
+                    tasks += todo.ElementAt(i).Key;
+                    tasks += " ";
+                }
+            }
+
+            return tasks;
+        }
+
+        public string IncompleteTasks()
+        {
+            string tasks = "";
+
+            for (int i = 0; i < todo.Count(); i++)
+            {
+                if (todo.ElementAt(i).Value == 0)
+                {
+                    tasks += todo.ElementAt(i).Key;
+                    tasks += " ";
+                }
+            }
+
+            return tasks;
+        }
+
+        public string TaskExists(string taskName)
+        {
+            string message = "";
+
+            if (todo.ContainsKey(taskName))
+                message = "Exists";
+            else
+                message = "Do not exist";
+
+            return message;
+        }
+
+        public bool RemovedTask(string taskName)
+        {
+            if (!todo.ContainsKey(taskName))
+                return false;
+
+            todo.Remove(taskName);
+            return true;
+        }
+
+        public string Alfabetically()
+        {
+            string sequence = "";
+            Dictionary<string, int> sorted = new Dictionary<string, int>();
+
+            sorted = todo.OrderBy(obj => obj.Key).ToDictionary(obj => obj.Key, obj => obj.Value);
+
+            for (int i = 0; i < sorted.Count(); i++)
+            {
+                sequence += sorted.ElementAt(i).Key;
+                sequence += " ";
+            }
+
+            return sequence;
+        }
+
+        public string BackwardsAlfabet()
+        {
+            string sequence = "";
+            Dictionary<string, int> sorted = new Dictionary<string, int>();
+
+            sorted = todo.OrderBy(obj => obj.Key).ToDictionary(obj => obj.Key, obj => obj.Value);
+
+            for (int i = sorted.Count() - 1; i >= 0; i--)
+            {
+                sequence += sorted.ElementAt(i).Key;
+                sequence += " ";
+            }
+
+            return sequence;
+        }
     }
 }
