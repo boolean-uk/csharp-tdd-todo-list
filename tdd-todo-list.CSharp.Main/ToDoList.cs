236b15fa7d5bf5919e05e529d362357a80cce544
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-01-12 15:22:58 +0100

    Finished exercise

--- tdd-todo-list.CSharp.Main/ToDoList.cs
+++ tdd-todo-list.CSharp.Main/ToDoList.cs
@@ -8,5 +8,79 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoList
     {
+        private Dictionary<string, bool> _toDoList = new Dictionary<string, bool>();
+        public Dictionary<string, bool> ToDoList { get { return _toDoList; } }
+
+        public void Add(string task, bool complete = false)
+        {
+            _toDoList.Add(task, complete);
+        }
+        public List<string> Show()
+        {
+            List<string> result = new List<string>();
+            foreach (KeyValuePair<string, bool> task in _toDoList)
+            {
+                if (task.Value) result.Add($"{task.Key}   COMPLETED");
+                else result.Add($"{task.Key}   NOT COMPLETED");
+            }
+            return result;
+        }
+
+        public string Search(string task, out bool complete)
+        {
+            if (_toDoList.ContainsKey(task))
+            {
+                complete = _toDoList[task];
+                return task;
+            }
+            complete = false;
+            return $"{task} doesn't exist";
+        }
+        public void Remove(string task)
+        {
+            _toDoList.Remove(task);
+        }
+        public void OrderAscending()
+        {
+            _toDoList.Order();
+        }
+
+        public void OrderDescending()
+        {
+            _toDoList.OrderDescending();
+        }
+        /// <summary>
+        /// Returns false when task doesn't exist, true otherwise
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public bool ToggleCompletion(string task)
+        {
+            if (!_toDoList.ContainsKey(task)) return false;
+            _toDoList[task] = !_toDoList[task];
+            return true;
+        }
+
+        public Dictionary<string, bool> CompletedTasks()
+        {
+            Dictionary<string, bool> result = new Dictionary<string, bool>();
+            foreach (KeyValuePair<string, bool> task in _toDoList)
+            {
+                if (task.Value) result.Add(task.Key, task.Value);
+            }
+            return result;
+        }
+        public Dictionary<string, bool> IncompleteTasks()
+        {
+            Dictionary<string, bool> result = new Dictionary<string, bool>();
+            foreach (KeyValuePair<string, bool> task in _toDoList)
+            {
+                if (!task.Value) result.Add(task.Key, task.Value);
+            }
+            return result;
+        }
+
     }
+
 }
--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -6,12 +6,128 @@ namespace tdd_todo_list.CSharp.Test
     [TestFixture]
     public class CoreTests
     {
+        TodoList core;
+
+        [SetUp]
+        public void SetUp()
+        {
+            core = new TodoList();
+        }
+        [Test]
+        public void AddTest()
+        {
+            core.Add("Clean");
+            Assert.That(core.ToDoList.Count, Is.EqualTo(1));
+            Assert.IsTrue(core.ToDoList.ContainsKey("Clean"));
+        }
+        [Test]
+        public void ShowTest()
+        {
+            core.Add("Finish exercise");
+            List<string> result = core.Show();
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.IsTrue(result.Contains("Finish exercise   NOT COMPLETED"));
+        }
+        [Test]
+        public void ToggleCompletionTest()
+        {
+            core.Add("Make card");
+            bool result = core.ToggleCompletion("Make card");
+            Assert.IsTrue(result);
+            Assert.IsTrue(core.ToDoList["Make card"]);
+        }
+        [Test]
+        public void ToggleCompletionTest1()
+        {
+            core.Add("Make card");
+            bool result = core.ToggleCompletion("Play guitar");
+            Assert.IsFalse(result);
+            Assert.IsFalse(core.ToDoList["Make card"]);
+        }
+        [Test]
+        public void ToggleCompletionTest2()
+        {
+            core.Add("Make card", true);
+            bool result = core.ToggleCompletion("Make card");
+            Assert.IsTrue(result);
+            Assert.IsFalse(core.ToDoList["Make card"]);
+        }
+
+        [Test] 
+        public void CompleteTasksTest()
+        {
+            core.Add("Eat", true);
+            core.Add("Sleep", false);
+            core.Add("Game", true);
+            core.Add("Repeat", false);
+            Dictionary<string, bool> result = core.CompletedTasks();
+            Assert.That(result, Is.EquivalentTo(new Dictionary<string, bool>() { { "Eat", true }, { "Game", true } }));
+        }
+        [Test]
+        public void IncompleteTasksTest()
+        {
+            core.Add("Eat", true);
+            core.Add("Sleep", false);
+            core.Add("Game", true);
+            core.Add("Repeat", false);
+            Dictionary<string, bool> result = core.IncompleteTasks();
+            Assert.That(result, Is.EquivalentTo(new Dictionary<string, bool>() { { "Sleep", false}, { "Repeat", false} }));
+        }
 
         [Test]
-        public void FirstTest()
+        public void SearchTest()
         {
-            TodoList core = new TodoList();
-            Assert.Pass();
+            core.Add("Eat", true);
+            core.Add("Sleep", false);
+            core.Add("Game", true);
+            core.Add("Repeat", false);
+            bool complete;
+            string result = core.Search("Game", out complete) ;
+            Assert.IsTrue(complete);
+            Assert.That(result, Is.EqualTo("Game"));
         }
+        [Test]
+        public void SearchDoesntExistTest()
+        {
+            core.Add("Eat", true);
+            core.Add("Sleep", false);
+            core.Add("Game", true);
+            core.Add("Repeat", false);
+            bool complete;
+            string result = core.Search("Work", out complete);
+            Assert.IsFalse(complete);
+            Assert.That(result, Is.EqualTo("Work doesn't exist"));
+        }
+        [Test]
+        public void RemoveTest()
+        {
+            core.Add("Eat", true);
+            core.Add("Sleep", false);
+            core.Add("Game", true);
+            core.Add("Repeat", false);
+            core.Remove("Repeat");
+            Assert.That(core.ToDoList, Is.EquivalentTo(new Dictionary<string, bool>() { { "Eat", true }, { "Sleep", false }, { "Game", true } }));
+        }
+        [Test]
+        public void OrderAscendingTest()
+        {
+            core.Add("Eat", true);
+            core.Add("Sleep", false);
+            core.Add("Game", true);
+            core.Add("Repeat", false);
+            core.OrderAscending();
+            Assert.That(core.ToDoList, Is.EqualTo(new Dictionary<string, bool>() { { "Eat", true }, { "Game", true}, { "Repeat", false} , { "Sleep" , false} }));
+        }
+        [Test]
+        public void OrderDescendingTest()
+        {
+            core.Add("Eat", true);
+            core.Add("Sleep", false);
+            core.Add("Game", true);
+            core.Add("Repeat", false);
+            core.OrderDescending();
+            Assert.That(core.ToDoList, Is.EqualTo(new Dictionary<string, bool>() {  { "Sleep", false }, { "Repeat", false }  ,  { "Game", true },{ "Eat", true } }));
+        }
+
     }
 }
