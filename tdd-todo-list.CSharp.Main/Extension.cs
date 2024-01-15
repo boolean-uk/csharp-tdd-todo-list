3f496099fee3fe5c81bf577ce1525c379192da2d
Jacob Chen Jensen <jacob.chen.jensen@dk.experis.com>
2024-01-15 13:42:34 +0100

    core finished, only part of extension

--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -6,7 +6,80 @@ using System.Threading.Tasks;
 
 namespace tdd_todo_list.CSharp.Main
 {
+    public struct TaskExtend
+    {
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+        public int ID { get; set; }
+        public string Date { get; set; }
+
+
+        public TaskExtend(int id, string name, string date)
+        {
+            Name = name;
+            ID = id;
+            IsCompleted = false;
+            Date = date;
+        }
+        public void setName(string name)
+        {
+            Name = name;
+        }
+
+    }
     public class TodoListExtension
     {
+
+        public List<TaskExtend> Tasks { get; set; }
+
+        public TodoListExtension()
+        {
+            Tasks = new List<TaskExtend>();
+        }
+
+        public bool addTask(int id, string taskName, string date)
+        {
+
+            Tasks.Add(new TaskExtend(id,taskName,date));
+
+            return true;
+        }
+
+        public TaskExtend getByID(int id)
+        {
+            TaskExtend result = new TaskExtend(0,"None","0");
+
+            foreach (TaskExtend task in Tasks)
+            {
+                if(task.ID == id)
+                {
+                    result = task;
+                }
+            }
+
+            return result;
+        }
+        public bool updateName(int id, string newName)
+        {
+            bool result = false;
+
+            foreach(TaskExtend task in Tasks)
+            {
+                if(task.ID == id)
+                {
+                    string tempDate = task.Date;
+                    Tasks.Remove(task);
+                    Tasks.Add(new TaskExtend(id, newName, tempDate));
+                    result = true;
+                }
+            }
+
+            Console.WriteLine(result);
+            return result;
+        }
+
+
+
+
     }
 }
--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -4,15 +4,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace tdd_todo_list.CSharp.Test
 {
+    [TestFixture]
     public class ExtensionTests
     {
-        private TodoListExtension _extension;
-        public ExtensionTests()
+
+        [Test]
+        public void FirstTest()
         {
-            _extension = new TodoListExtension();
+            TodoListExtension extend = new TodoListExtension();
+
+            extend.addTask(111, "Cleaning", "July 07");
+
+            Assert.IsTrue(extend.getByID(111).Name == "Cleaning");
+            Assert.IsTrue(extend.getByID(111).Date == "July 07");
+            Assert.IsTrue(extend.getByID(111).IsCompleted == false);
         }
+        [Test]
+        public void SecondTest()
+        {
+            TodoListExtension extend = new TodoListExtension();
+
+            extend.addTask(111, "Cleaning", "July 07");
+
+            Assert.IsTrue(extend.getByID(0).Name == "None");
+
+        }
+        [Test]
+        public void ThirdTest()
+        {
+            TodoListExtension extend = new TodoListExtension();
+
+            extend.addTask(111, "Cleaning", "July 07");
+            extend.updateName(111, "Cooking");
+
+
+            Assert.IsTrue(extend.getByID(111).Name == "Cooking");
+
+        }
+
+
+
     }
 }
