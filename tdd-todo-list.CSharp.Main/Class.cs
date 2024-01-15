2f10023b510e6747827b04a5b4f3a294a58b59ba
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2024-01-15 13:00:44 +0100

    Henrik Rosenkilde c# EX3

---
+++ tdd-todo-list.CSharp.Main/Class.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace tdd_todo_list.CSharp.Main
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+
+        public int Id { get; set; }
+
+        public Task(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            IsComplete = false; // By default, tasks are incomplete
+            Id = Id;
+        }
+
+        public void ToggleStatus()
+        {
+            IsComplete = !IsComplete;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {Title}, Description: {Description}, Status: {(IsComplete ? "Complete" : "Incomplete")}";
+        }
+    }
+}
