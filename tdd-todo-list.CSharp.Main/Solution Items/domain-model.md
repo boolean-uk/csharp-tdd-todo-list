9dbc44d4cf8856e4e2adfcf57f91a259cc1c71a8
Kanthee Kosolyuthasarn <Kanthee>
2024-01-15 11:48:14 +0100

    Kanthee Kosolyuthasarn - C#3 - 15.01.2024

---
+++ tdd-todo-list.CSharp.Main/Solution Items/domain-model.md
@@ -0,0 +1,99 @@
+﻿# Domain model for TODO list.
+
+1. I want to add tasks to my todo list.
+
+2. I want to see all the tasks in my todo list.
+
+3. I want to change the status of a task between incomplete and complete.
+
+4. I want to be able to get only the complete tasks.
+
+5. I want to be able to get only the incomplete tasks.
+
+6. I want to search for a task and receive a message that says it wasn't found if it doesn't exist.
+
+7. I want to remove tasks from my list.
+
+8. I want to see all the tasks in my list ordered alphabetically in ascending order.
+
+9. I want to see all the tasks in my list ordered alphabetically in descending order.
+
+## Domain Models In here
+```
+1. I want to add tasks to my todo list.
+
+```
+
+| Classes         | Methods                                            | Scenario										| Outputs          |
+|-----------------|----------------------------------------------------|------------------------------------------------|------------------|
+|`TodoList`		  |	`Add(string task)`				                   | add string task to Dictionary<strig,string>	| void             |
+
+```
+2. I want to see all the tasks in my todo list.
+```
+
+| Classes         | Methods                                            | Scenario							 | Outputs          |
+|-----------------|----------------------------------------------------|-------------------------------------|------------------|
+|`TodoList`		  |	`displayList(string sort)`						   | Display Dictionary<strig,string>	 | List<string>     |
+
+```
+3. I want to change the status of a task between incomplete and complete.
+```
+
+| Classes         | Methods                                            | Scenario                        | Outputs          |
+|-----------------|----------------------------------------------------|---------------------------------|------------------|
+|`TodoList`		  |	`changeStatus(string task, string status)`         |Change value based on key task	 | void             |
+
+
+```
+4. I want to be able to get only the complete tasks.
+```
+
+| Classes         | Methods                                            | Scenario							 | Outputs          |
+|-----------------|----------------------------------------------------|-------------------------------------|------------------|
+|`TodoList`		  |	`displayList(string sort)`				           | Display Dictionary<strig,string>	 | List<string>     |
+
+
+```
+5. I want to be able to get only the incomplete tasks.
+```
+
+| Classes         | Methods                                            | Scenario							 | Outputs          |
+|-----------------|----------------------------------------------------|-------------------------------------|------------------|
+|`TodoList`		  |	`displayList(string sort)`				           | Display Dictionary<strig,string>	 | List<string>     |
+
+
+```
+6. I want to search for a task and receive a message that says it wasn't found if it doesn't exist.
+```
+
+| Classes         | Methods                                            | Scenario								   | Outputs          |
+|-----------------|----------------------------------------------------|-------------------------------------------|------------------|
+|`TodoList`		  |	`searchingTask(string task)`					   | Display task i in the List, else *message*| string           |
+
+
+```
+7. I want to remove tasks from my list.
+```
+
+| Classes         | Methods                                            | Scenario                        | Outputs          |
+|-----------------|----------------------------------------------------|---------------------------------|------------------|
+|`TodoList`		  |	`remove(string task)`				               | remove string task from the list| void             |
+
+
+```
+8. I want to see all the tasks in my list ordered alphabetically in ascending order.
+```
+
+| Classes         | Methods                                            | Scenario                        | Outputs          |
+|-----------------|----------------------------------------------------|---------------------------------|------------------|
+|`TodoList`		  |	`sort(Dictionary<strig,string> list, string order)`| Sort the list ascending order	 | List<string>     |
+
+
+```
+9. I want to see all the tasks in my list ordered alphabetically in descending order.
+```
+
+| Classes         | Methods                                            | Scenario                        | Outputs          |
+|-----------------|----------------------------------------------------|---------------------------------|------------------|
+|`TodoList`		  | `sort(Dictionary<strig,string> list, string order)`| Sort the list descending order	 | List<string>     |
--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -8,10 +8,231 @@ namespace tdd_todo_list.CSharp.Test
     {
 
         [Test]
-        public void FirstTest()
+        public void addTask()
         {
+            //Arrange:
             TodoList core = new TodoList();
+
+
+            // Act:
+            core.addTask("Help me");
+            core.addTask("Help me2");
+            Dictionary<string, string> testList = new Dictionary<string, string> {
+                {"Help me","incompleted" },
+                {"Help me2","incompleted" }
+            };
+            var result = core._toDoList;
+
+            //Assert:
+            Assert.AreEqual(testList, result);
             Assert.Pass();
         }
+
+        [Test]
+        public void testRemoveTask()
+        {
+            //Arrange:
+            TodoList core = new TodoList();
+
+
+            // Act:
+            core.addTask("Help me");
+            core.addTask("Help me2");
+            Dictionary<string, string> testList = new Dictionary<string, string> {
+                {"Help me","incompleted" },
+                
+            };
+            core.removeTask("Help me2");
+            var result = core._toDoList;
+
+            //Assert:
+            Assert.AreEqual(testList, result);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void testSeraching1()
+        {
+            //Arrange:
+            TodoList core = new TodoList();
+
+
+            // Act:
+            core.addTask("Help me");
+            core.addTask("Help me2");
+            Dictionary<string, string> testList = new Dictionary<string, string> {
+                {"Help me","incompleted" },
+                {"Help me2","incompleted" }
+            };
+            var result = core._toDoList;
+
+            //Assert:
+            Assert.IsTrue(core.searchingTask("Help me") == "In the toDoList");
+            Assert.Pass();
+        }
+
+        [Test]
+        public void testSeraching2()
+        {
+            //Arrange:
+            TodoList core = new TodoList();
+
+
+            // Act:
+            core.addTask("Help me");
+            core.addTask("Help me2");
+            Dictionary<string, string> testList = new Dictionary<string, string> {
+                {"Help me","incompleted" },
+                {"Help me2","incompleted" }
+            };
+            var result = core._toDoList;
+
+            //Assert:
+            Assert.IsTrue(core.searchingTask("Help meee") == "it wasn't found");
+            Assert.Pass();
+        }
+
+        [Test]
+        public void testChangeStatus()
+        {
+            //Arrange:
+            TodoList core = new TodoList();
+
+
+            // Act:
+            core.addTask("Help me");
+            core.addTask("Help me2");
+            Dictionary<string, string> testList = new Dictionary<string, string> {
+                {"Help me","completed" },
+                {"Help me2","incompleted" }
+            };
+            core.changeStatus("Help me", "completed");
+            var result = core._toDoList;
+
+            //Assert:
+            Assert.AreEqual(testList, result);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void testSortCompleted()
+        {
+            //Arrange:
+            TodoList core = new TodoList();
+
+
+            // Act:
+            core.addTask("Help me");
+            core.addTask("Help me2");
+            core.addTask("Help me3");
+            core.addTask("Help me4");
+            core.addTask("Help me5");
+            List<string> testList = new List<string> {
+                {"Help me3"},
+                {"Help me5" },
+                {"Help me" },
+                {"Help me2" },
+                {"Help me4" },
+
+            };
+            core.changeStatus("Help me5", "completed");
+            core.changeStatus("Help me3", "completed");
+            var result = core.showTask("completed");
+
+            //Assert:
+            Assert.AreEqual(testList, result);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void testSortInCompleted()
+        {
+            //Arrange:
+            TodoList core = new TodoList();
+
+
+            // Act:
+            core.addTask("Help me");
+            core.addTask("Help me2");
+            core.addTask("Help me3");
+            core.addTask("Help me4");
+            core.addTask("Help me5");
+            List<string> testList = new List<string> {
+                {"Help me"},
+                {"Help me2" },
+                {"Help me4" },
+                {"Help me3" },
+                {"Help me5" }
+
+            };
+            core.changeStatus("Help me5", "completed");
+            core.changeStatus("Help me3", "completed");
+            var result = core.showTask("incompleted");
+
+            //Assert:
+            Assert.AreEqual(testList, result);
+            Assert.Pass();
+        }
+
+
+        [Test]
+        public void testSortAscending()
+        {
+            //Arrange:
+            TodoList core = new TodoList();
+
+
+            // Act:
+            core.addTask("EHelp me");
+            core.addTask("DHelp me2");
+            core.addTask("CHelp me3");
+            core.addTask("BHelp me4");
+            core.addTask("AHelp me5");
+            List<string> testList = new List<string> {
+                {"AHelp me5"},
+                {"BHelp me4" },
+                {"CHelp me3" },
+                {"DHelp me2" },
+                {"EHelp me" }
+
+            };
+      
+            var result = core.showTask("ascending");
+
+            //Assert:
+            Assert.AreEqual(testList, result);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void testSortDescending()
+        {
+            //Arrange:
+            TodoList core = new TodoList();
+
+
+            // Act:
+            core.addTask("EHelp me");
+            core.addTask("DHelp me2");
+            core.addTask("CHelp me3");
+            core.addTask("BHelp me4");
+            core.addTask("AHelp me5");
+            List<string> testList = new List<string> {
+                {"EHelp me" },
+                {"DHelp me2" },
+                {"CHelp me3" },
+                {"BHelp me4" },
+                {"AHelp me5"}
+
+            };
+
+            var result = core.showTask("descending");
+
+            //Assert:
+            Assert.AreEqual(testList, result);
+            Assert.Pass();
+        }
+
+
     }
 }
