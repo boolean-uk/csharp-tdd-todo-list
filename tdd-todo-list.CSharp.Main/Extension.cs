01ab4c5b982d12ed1b61fc751cc7d9fe27dcce3b
guro18 <rollgustav95@gmail.com>
2024-01-15 16:04:45 +0100

    TodoList Gustav Roll

--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,88 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoListExtension
     {
+        //List that contans arrays of strings, where every string represents a task
+        private List<string[]> tasks = new List<string[]>();
+
+        //Add new task with all attributes
+        public void addTask(string taskName, string status) 
+        {
+            string[] task = new string[]
+            {
+                taskName, //TaskName
+                Guid.NewGuid().ToString(), //ID
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm"), //TimeStamp
+                status //Status
+            };
+            tasks.Add(task);
+            
+        }
+
+        //getID of task
+        public string getID(string taskName) 
+        {
+            string ID = "";
+            foreach (var task in tasks) 
+            {
+                if (task[0] == taskName) 
+                {
+                    ID = task[1];
+                }
+            }
+            return ID;
+        }
+
+        //Get task by provide ID
+        public string[] getTaskByID(string ID) 
+        {
+
+            foreach (var task in tasks) 
+            {
+                if (task[1] == ID) 
+                {
+                    return task;
+                }
+            }
+            return new string[] { "ID does not exist" };
+
+        }
+
+        //update Taskname by provide ID
+        public void updateTaskNameByID(string ID, string taskName) 
+        {
+            foreach (var task in tasks)
+            {
+                if (task[1] == ID) 
+                {
+                    task[0] = taskName;
+                }
+            }
+        }
+
+        //update TaskStatus by provide ID
+        public void updateTaskStatusByID (string ID, string status) 
+        {
+            foreach(var task in tasks) 
+            {
+                if (task[1] == ID) 
+                {
+                    task[3] = status;
+                }
+            }
+        }
+
+        //Show all tasks & timestamps related
+        public string[] showTimeStamps() 
+        {
+            List<string> taskAndTimeStamps = new List<string>();
+
+            foreach (var task in tasks) 
+            {
+                taskAndTimeStamps.Add(task[0] + ":" + task[2]);
+            }
+            
+            return taskAndTimeStamps.ToArray();
+
+        }
     }
 }
--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -4,15 +4,97 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace tdd_todo_list.CSharp.Test
 {
+    [TestFixture]
     public class ExtensionTests
     {
+        
         private TodoListExtension _extension;
+
         public ExtensionTests()
         {
             _extension = new TodoListExtension();
+
+        }
+
+        // Test getID method
+        [Test]
+        public void getTaskByIDTest() 
+        {
+            _extension.addTask("task1", "0");
+
+            string ID = _extension.getID("task1");
+
+            string[] expectedArray = {"task1", ID, "0"};
+
+            string[] actualArray = _extension.getTaskByID(ID);
+
+            Assert.AreEqual(expectedArray[0], actualArray[0]);
+            Assert.AreEqual(expectedArray[1], actualArray[1]);
+            Assert.AreEqual(expectedArray[2], actualArray[3]);
+
+        }
+
+        // Test updateTaskName method
+        [Test]
+        public void updateTaskNameTest() 
+        {
+            _extension.addTask("task1", "0");
+
+            string ID = _extension.getID("task1");
+
+            string[] expectedArray = { "newTask1", ID, "0" };
+
+            _extension.updateTaskNameByID(ID, "newTask1");
+
+            string[] actualArray = _extension.getTaskByID(ID);
+
+            Assert.AreEqual(expectedArray[0], actualArray[0]);
+
+        }
+
+        // Test UpdateTaskStatus method
+        [Test]
+        public void updateTaskStatusByIDTest() 
+        {
+            _extension.addTask("task1", "0");
+
+            string ID = _extension.getID("task1");
+
+            string [] actualArray = _extension.getTaskByID(ID);
+
+            Assert.AreEqual(actualArray[3], "0");
+
+            _extension.updateTaskStatusByID(ID, "1");
+
+            Assert.AreEqual(actualArray[3], "1");
+
+            _extension.updateTaskStatusByID(ID, "0");
+
+            Assert.AreEqual(actualArray[3], "0");
+        }
+
+        // Test ShowTimeStamp Function
+        [Test]
+        public void showTimeStampsTest() 
+        {
+            string currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+
+            _extension.addTask("task1", "0");
+            _extension.addTask("task2", "1");
+            _extension.addTask("task3", "0");
+
+            string[] taskAndTimeStamps = 
+                {
+                "task1:" + currentTime,
+                "task2:" + currentTime,
+                "task3:" + currentTime
+                };
+
+            Assert.AreEqual(taskAndTimeStamps, _extension.showTimeStamps());
         }
     }
 }
