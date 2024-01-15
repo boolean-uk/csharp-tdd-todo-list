056b2456a4b6a0256cff3d91c60fb0378a14c477
Philip Morud <philip.morud@no.experis.com>
2024-01-15 13:45:57 +0100

    Added extension tests and domain model

--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -8,5 +8,38 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoListExtension
     {
+
+        private List<IdTask> _tasks = new List<IdTask>();
+
+        public List<IdTask> Tasks { get => _tasks; }
+
+        public TodoListExtension() { }
+
+        public IdTask Add(string name)
+        {
+            IdTask task = new IdTask(name);
+            _tasks.Add(task);
+            return task;
+        }
+
+        public IdTask Get(string id)
+        {
+            return new IdTask("");
+        } 
+
+        public void Rename(string id, string name)
+        {
+
+        }
+
+        public void Complete(string id)
+        {
+
+        }
+
+        public string PrintCreated()
+        {
+            return "";
+        }
     }
 }
