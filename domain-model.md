39fb3cb98a5fd74ff5d3884a8655bb098bfcead0
llllllll-l <vincent.werelius@gmail.com>
2024-01-15 11:11:54 +0100

    Core done

--- domain-model.md
+++ domain-model.md
@@ -1,22 +1,26 @@
 ```C#
 struct Task:
-    int _id
-    string task/desc
-    bool isCompleted
+    public int _id
+    punlic string task/desc
+    public bool isCompleted
 
 class ToDo:
     MEMBERS:
-        private List<Task> tasks;
+        public List<Task> tasks;
         private int _idHolder;
 
     METHODS:
-        AddTask(string name)                         | generate a new Task();
-        RemoveTask(int _id)                         | 
-        DisplayTasks()                          | return list of tasks;
-        ChangeStatus(int _id, bool true/false)  | changes the state of given task;
-        GetCompletedTasks()                     | returns only the tasks that has status true;
-        GetIncompletedTaska()                   | returns only the tasks that has status false;         
-        SearchTask(int _id)                         | retuen Task or throe new error message;   
-        GetOrderedList(char 'a' or char 'd')              | returns ascending or descending depending on input
+        public AddTask(string name)                         | return true if new Task() is generated else return false  
+        public RemoveTask(int _id)                         | return true if tsk is removed by _id, else false
+        public DisplayTasks()                          | return list of tasks;
+        public ChangeStatus(int _id, bool true/false)  | changes the state of given task;
+        public GetCompletedTasks()                     | return only the tasks that has status true;
+        public GetIncompletedTaska()                   | return only the tasks that has status false;         
+        public SearchTask(int _id)                         | return Task or throe new error message;   
+        public GetOrderedList(char 'a' or char 'd')              | returns ascending or descending depending on input
+        
+    HELPER:
+        private GetAscendingOrder() | return a ordered list in ascending order
+        private GetDescendingOrder() | return a ordered list in descending order
 
 ```
--- tdd-todo-list.CSharp.Main/ToDoList.cs
+++ tdd-todo-list.CSharp.Main/ToDoList.cs
@@ -8,5 +8,122 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoList
     {
+
+        public List<Task> tasks { get; set; }
+        private int _idHolder = 0;
+        public struct Task {
+            public int _id;
+            public string task;
+            public bool isCompleted;
+
+            public Task(int _id, string task, bool isCompleted) {
+                this._id = _id;
+                this.task = task;
+                this.isCompleted = isCompleted;
+            }
+        }
+
+        public TodoList() {
+            tasks = new List<Task>();
+        }
+
+        public bool AddTask(string task) {
+            int tmp = tasks.Last()._id;
+            tasks.Add(new Task(++tmp, task, false));
+
+            return true;
+        }
+
+        public bool RemoveTask(int v)
+        {
+            foreach (Task item in tasks) {
+                if (item._id == v) {
+                    tasks.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<string> DisplayTasks() {
+            List<string> res = new List<string>();
+
+            foreach (Task item in tasks) {
+                res.Add($"{item.task} is completed: {item.isCompleted}");
+            }
+
+            return res;
+        }
+
+        public bool ChangeStatus(int index, bool state) {
+
+            for (int i = 0; i < tasks.Count; i++) {
+                if (tasks[i]._id == index && tasks[i].isCompleted != state) {
+                    Task tmpTask = tasks[i];
+                    tmpTask.isCompleted = state;
+                    tasks[i] = tmpTask;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<string> GetCompletedTasks()
+        {
+            List<string> completedTasks = new List<string>();
+            foreach (Task item in tasks) {
+                if (item.isCompleted == true) {
+                    completedTasks.Add(item.task);
+                }
+            }
+
+            return completedTasks;
+        }
+
+        public List<string> GetIncompletedTaska()
+        {
+            List<string> inCompletedTasks = new List<string>();
+            foreach (Task item in tasks) {
+                if (item.isCompleted == false) {
+                    inCompletedTasks.Add(item.task);
+                }
+            }
+
+            return inCompletedTasks;
+        }
+
+        public Task SearchTask(int _id) {
+            foreach (Task item in tasks) {
+                if (item._id == _id) {
+                    return item;
+                }
+            }
+            
+            throw new InvalidDataException();
+        }
+
+        public List<string> GetOrderedList(char order)
+        {
+            if (order.Equals('a')) {
+                return AscendingOrder(tasks);
+            } else if (order.Equals('d')) {
+                return DescendingOrder(tasks);
+            }
+            else {
+                throw new InvalidOperationException();
+            }
+        }
+
+        private List<string> DescendingOrder(List<Task> tasks) {
+            List<Task> orderedTasks = tasks.OrderByDescending(t => t.task).ToList();
+            List<string> orderedTaskNames = orderedTasks.Select(t => t.task).ToList();
+            return orderedTaskNames;
+        }
+
+        private List<string> AscendingOrder(List<Task> tasks) {
+            List<Task> orderedTasks = tasks.OrderBy(t => t.task).ToList();
+            List<string> orderedTaskNames = orderedTasks.Select(t => t.task).ToList();
+            return orderedTaskNames;
+        }
     }
 }
