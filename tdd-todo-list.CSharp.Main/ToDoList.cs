28b063742c4cb9b77f75747ff727a5e6ca65c963
SBjer1 <sebastian.engan@no.experis.com>
2024-01-15 15:44:06 +0100

    Done with core

--- tdd-todo-list.CSharp.Main/ToDoList.cs
+++ tdd-todo-list.CSharp.Main/ToDoList.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,125 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoList
     {
+        public Dictionary<string, bool> Todos = new Dictionary<string, bool>();
+       
+
+        public void AddTask(string task)
+        {
+            bool completed = false;
+            
+            if(!Todos.ContainsKey(task)) 
+            {
+                Todos.Add(task.ToLower(), completed);
+            }
+
+        }
+
+
+        public int showTodoList()
+        {
+            int count = 0;
+            
+            foreach(string task in Todos.Keys) 
+            {
+                count ++;
+                Console.WriteLine($"{task} is completed: {Todos[task]}");
+            }
+
+            return count;
+        }
+        public void changeTaskStatus(string task)
+        {
+            if (Todos.ContainsKey(task))
+            {
+                Todos[task] = true;
+                
+            }
+        }
+
+        public int showCompletedTasks()
+        {
+            int count = 0;
+            foreach(var task in Todos)
+            {
+                if (task.Value == true) 
+                {
+                    count++;
+                    Console.WriteLine($"{task.Key} is completed: {task.Value}");
+                    
+                }
+               
+            }
+            return count;
+        }
+
+        public int showIncompletedTasks()
+        {
+            int count = 0;
+            foreach (var task in Todos)
+            {
+                if (task.Value == false)
+                {
+                    count++;
+                    Console.WriteLine($"{task.Key} is completed: {task.Value}");
+
+                }
+
+            }
+            return count;
+        }
+
+       
+
+        public string search(string task)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (!Todos.ContainsKey(task))
+            {
+                sb.Append("Task not in list");
+                Console.WriteLine(sb);
+                
+            }
+            return sb.ToString();
+        }
+
+        public bool RemoveTask(string task)
+        {
+            if(Todos.ContainsKey(task))
+            {
+                Todos.Remove(task);
+                return true;
+            }
+
+            return false;
+        }
+
+        public string SortAlphaAsc()
+        {
+            var sortedDict = from entry in Todos orderby entry.Key ascending select entry;
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in sortedDict)
+            {
+                sb.Append(item);
+                Console.WriteLine(item.ToString());
+            }
+
+            return sb.ToString();
+        }
+
+        public string SortDecs()
+        {
+            var sortedDict = from entry in Todos orderby entry.Key descending select entry;
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in sortedDict)
+            {
+                sb.Append(item);
+                Console.WriteLine(item.ToString());
+            }
+
+            return sb.ToString();
+        }
     }
 }
--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -1,5 +1,7 @@
 ﻿using tdd_todo_list.CSharp.Main;
 using NUnit.Framework;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace tdd_todo_list.CSharp.Test
 {
@@ -12,6 +14,147 @@ namespace tdd_todo_list.CSharp.Test
         {
             TodoList core = new TodoList();
             Assert.Pass();
+        }
+
+        [TestCase ("Laundry")]
+        [TestCase ("Dishes")]
+        
+        
+        public void AddTaskToTodo(string task)
+        {
+            //Arrange
+            TodoList core = new TodoList();
+            
+            //Act 
+            core.AddTask(task);
+
+            //Assert
+            Assert.IsTrue(core.Todos.Count == 1);   
+        }
+
+        [Test]
+        public void ShowAllTasks()
+        {
+            //Arrange
+            TodoList core = new TodoList();
+
+            //Act 
+            core.AddTask("Laundry");
+            core.showTodoList();
+            
+            //Assert
+            Assert.AreEqual(1, core.Todos.Count);
+        }
+
+        [Test]
+        
+        public void changeTaskStatus()
+        {
+            //Arrange
+            TodoList core = new TodoList();
+            string task = "laundry";
+
+            //Act 
+
+            core.AddTask (task);
+            core.changeTaskStatus(task);
+
+            //Assert
+            Assert.IsTrue(core.Todos[task] == true);
+        }
+
+        [Test]
+
+        public void showCompletedTasks()
+        {
+            //Arrange
+            TodoList core = new TodoList();
+
+            //Act 
+            core.AddTask("Laundry");
+            core.changeTaskStatus("Laundry");
+
+            //Assert
+            Assert.AreEqual(1, core.Todos.Count);
+        }
+
+        [Test]
+        public void showIncompletedTasks()
+        {
+            //Arrange
+            TodoList core = new TodoList();
+
+            //Act 
+            core.AddTask("Laundry");
+            
+
+            //Assert
+            Assert.AreEqual(1, core.Todos.Count);
+        }
+
+        [TestCase("Dishes")] 
+
+        public void NotFound(string task)
+        {
+            //Arrange
+            TodoList core = new TodoList();
+            
+            string msg = "Task not in list";
+            //Act
+            core.AddTask(task);
+            string searched = core.search(task);
+
+            //Assert
+            Assert.AreEqual(searched, msg);
+        }
+
+        [TestCase("Dishes")] 
+
+        public void RemoveTask(string task)
+        {
+            //Arrange
+            TodoList core = new TodoList(); 
+            
+            //Act
+            core.AddTask("Dishes");
+            core.RemoveTask(task);
+
+            //Assert
+            Assert.IsTrue(!core.Todos.ContainsKey(task));
+        }
+
+        [Test]
+
+        public void SortAlphabeticalAsc()
+        {
+            //Arrange
+            TodoList core = new TodoList();
+            string ordered = "[dishes, False][wash car, False]";
+
+            //Act
+            core.AddTask("Wash car");
+            core.AddTask("Dishes");
+            string sorted = core.SortAlphaAsc();
+
+            //Arrange
+            Assert.AreEqual(sorted, ordered);
+        }
+
+        [Test]
+
+        public void SortDescending()
+        {
+            //Arrange
+            TodoList core = new TodoList();
+            string ordered = "[wash car, False][dishes, False]";
+
+            //Act
+            core.AddTask("Dishes");
+            core.AddTask("Wash car");
+            string sorted = core.SortDecs();
+
+            //Arrange
+            Assert.AreEqual(sorted, ordered);
         }
     }
 }
