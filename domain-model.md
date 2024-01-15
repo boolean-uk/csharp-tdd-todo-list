884b90a8c25e02c9a094d8cb221082b45b4557df
Louis <louisshrestha0412@outlook.com>
2024-01-15 12:44:04 +0100

    Tasks completed

---
+++ domain-model.md
@@ -0,0 +1,17 @@
+#Domain Models In Here
+
+Exercise 1)
+| Classes         | Methods                                                  | Outputs                                        |
+|-----------------|----------------------------------------------------------|------------------------------------------------|
+| `Todo`          | `public bool Add(string message)`                        | Adding something into the to do list           |
+| `Todo, Item`    | `public List<Item> View()`                               | Adding something into the to do list           |
+| `Todo`          | `public bool Change(string task, bool state)`            | Adding something into the to do list           |
+| `Todo, Item`    | `public List<Item> CompletedTasks()`                     | Adding something into the to do list           |
+| `Todo, Item`    | `public List<Item> IncompletedTasks()`                   | Adding something into the to do list           |
+| `Todo`          | `public string Search(string search, out string result)` | Adding something into the to do list           |
+| `Todo`          | `public bool Remove (string taskName)`                   | Adding something into the to do list           |
+| `Todo, Item`    | `public List<Item> AscOrder()`                           | Adding something into the to do list           |
+| `Todo, Item`    | `public List<Item> DescOrder()`                          | Adding something into the to do list           |
+
+
+
--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -5,8 +5,59 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace tdd_todo_list.CSharp.Main
-{
+{    
     public class TodoListExtension
     {
+        public List<Task> tasks { get; set; } = new List<Task>();
+
+        public bool ChangeTaskStatus(Guid id, bool newStatus)
+        {
+            Task? task = tasks.Find(t => t.Id == id);
+            if (task != null)
+            {
+                task.Completed = newStatus;
+                return true;
+            }
+            return false;
+        }
+
+        public Task GetTimeOfCreation(Guid id)
+        {
+            try
+            {
+                Task? task = tasks.Find(t => t.Id == id);
+                return task ?? null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public Task? Search(Guid id)
+        {
+            try 
+            {
+                Task? task = tasks.Find(t => t.Id == id);
+                return task ?? null;
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public bool Update(Guid id, string newTask)
+        {
+            Task? task = tasks.Find(t => t.Id == id);
+            if (task != null) 
+            {
+                task.Message = newTask;
+                return true;
+            }
+            return false;
+        }
     }
 }
