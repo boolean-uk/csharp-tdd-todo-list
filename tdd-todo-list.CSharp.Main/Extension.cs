675aa34cd41b608c971222c7df013ad1c5210b89
Sebank <sebank@gmail.com>
2024-01-15 09:47:07 +0100

    Sebastian Ankill C#3

--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace tdd_todo_list.CSharp.Main
 {
-    public class TodoListExtension
+    public class TodoListExtension : TodoList
     {
+        public TodoListExtension() :base() {  }
     }
 }
--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -4,15 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+
+/*
+I want to be able to get a task by a unique ID.
+I want to update the name of a task by providing its ID and a new name.
+I want to be able to change the status of a task by providing its ID.
+I want to be able to see the date and time that I created each task.
+ */
 
 namespace tdd_todo_list.CSharp.Test
 {
+    [TestFixture]
     public class ExtensionTests
     {
         private TodoListExtension _extension;
-        public ExtensionTests()
+        [Test]
+        public void IDTest()
         {
-            _extension = new TodoListExtension();
+            TodoList core = new TodoListExtension();
+            core.addTask("name");
         }
     }
 }
