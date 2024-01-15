2aff0e8edff486c276b3b179611044b018cb2903
llllllll-l <vincent.werelius@gmail.com>
2024-01-15 13:14:01 +0100

    Extension and core tested and done

--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -14,11 +14,14 @@ namespace tdd_todo_list.CSharp.Main
             public int _id;
             public string task;
             public bool state;
+            
+            public string dateTime;
 
             public Task(int _id, string task, bool state) {
                 this._id = _id;
                 this.task = task;
                 this.state = state;
+                dateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             }
         }
 
@@ -27,14 +30,44 @@ namespace tdd_todo_list.CSharp.Main
             tasks = new List<Task>();
         }
 
+        public void UpdateData(int _id, string task) {
+             for (int i = 0; i < tasks.Count; i++) {
+                if (tasks[i]._id == _id && tasks[i].task != task) {
+                    Task tmpTask = tasks[i];
+                    tmpTask.task = task;
+                    tasks[i] = tmpTask;
+                }
+            }
+        }
 
+        public Task GetTask(int _id) {
+            foreach (Task item in tasks) {
+                if (item._id == _id) {
+                    return item;
+                }
+            }
+            throw new InvalidDataException();
+        }
 
+        public void ChangeStatus(int _id, bool state) {
+             for (int i = 0; i < tasks.Count; i++) {
+                if (tasks[i]._id == _id && tasks[i].state != state) {
+                    Task tmpTask = tasks[i];
+                    tmpTask.state = state;
+                    tasks[i] = tmpTask;
+                }
+            }
+        }
 
-
-
-
-
-
-
+        public List<string> DisplayList() {
+            List<string> result = new List<string>();
+            
+            foreach (Task item in tasks) {
+                result.Add($"{item.task} created at: {item.dateTime}");
+            }
+            
+            return result;
+            
+        }
     }
 }
--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -23,19 +23,56 @@ namespace tdd_todo_list.CSharp.Test
             _extension.tasks.Add(new TodoListExtension.Task(4, "task4", false));
             _extension.tasks.Add(new TodoListExtension.Task(5, "task5", true));
         }
-        //* - I want to be able to get a task by a unique ID.
-        //*- I want to update the name of a task by providing its ID and a new name.
-        //*- I want to be able to change the status of a task by providing its ID.
-        //*- I want to be able to see the date and time that I created each task.
+        
+        
+        
 
         [Test]
-        public void UpdateData() { //*- I want to update the name of a task by providing its ID and a new name.
+        public void TestGetTaskById() { //* - I want to be able to get a task by a unique ID.
+            TodoListExtension.Task foundTask = _extension.GetTask(2);
+
+            Assert.IsNotNull(foundTask);
+            Assert.AreEqual(2, foundTask._id);
+        }
+
+        [Test]
+        public void TestUpdateData() { //*- I want to update the name of a task by providing its ID and a new name.
             _extension.UpdateData(2, "Feed Bob my cat");
 
             TodoListExtension.Task updatedTask = _extension.tasks.FirstOrDefault(t => t._id == 2);
 
-            Assert.NotNull()
+            Assert.NotNull(updatedTask);
+            Assert.AreEqual("Feed Bob my cat", updatedTask.task);
         }
+
+        [Test]
+        public void TestChangeState() { //*- I want to be able to change the status of a task by providing its ID.
+            _extension.ChangeStatus(2, false);
+
+    
+            TodoListExtension.Task updatedTask = _extension.tasks.FirstOrDefault(t => t._id == 2);
+
+            Assert.NotNull(updatedTask, "Task with the specified ID should exist");
+            Assert.AreEqual(false, updatedTask.state, "Task state should be updated");
+        }
+        
+
+        [Test]
+        public void TestTimeStamp() { //*- I want to be able to see the date and time that I created each task.
+            List<string> tasks = _extension.DisplayList();
+
+    
+            foreach (string task in tasks) {
+                string[] taskParts = task.Split(new[] { "created at: " }, StringSplitOptions.RemoveEmptyEntries);
+
+                Assert.IsTrue(taskParts.Length >= 2);
+
+                string timestampPart = taskParts[1].Trim();
+
+                Assert.IsTrue(DateTime.TryParseExact(timestampPart, "yyyy-MM-dd HH:mm:ss", null, System.Globalization.DateTimeStyles.None, out _));
+            }
+        }
+        
         
     }
 }
