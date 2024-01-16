5a32bbfa39e395e47c916124b84a5f22c20b1f50
Philip Morud <philip.morud@no.experis.com>
2024-01-16 08:19:43 +0100

    Added files that were left out of commit

--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tdd_todo_list.CSharp.Main;
 
 namespace tdd_todo_list.CSharp.Main
 {
---
+++ tdd-todo-list.CSharp.Main/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_todo_list.CSharp.Main
+{
+    public class Task
+    {
+        string _name;
+        bool _isComplete;
+
+        public string Name { get => _name; set => _name = value; }
+        public bool Completed { get => _isComplete; set => _isComplete = value; }
+        public Task(string name) { 
+            _name = name;
+            _isComplete = false;
+        }
+
+
+    }
+}
