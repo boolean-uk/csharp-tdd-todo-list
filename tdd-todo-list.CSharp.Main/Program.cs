794c0327177b279bb9f653ab99c55f204751b77b
Rose van der Giessen <Rose van der Giessen>
2024-01-30 15:22:35 +0100

    Domain model + core complete

--- tdd-todo-list.CSharp.Main/Program.cs
+++ tdd-todo-list.CSharp.Main/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using tdd_todo_list;
+using tdd_todo_list.CSharp.Main;
+
 Console.WriteLine("Hello, World!");
+tdd_todo_list.Task task = new tdd_todo_list.Task("Do groceries");
+tdd_todo_list.Task task2 = new tdd_todo_list.Task("Clean");
+
+TodoList todoList = new TodoList();
+todoList.AddTask(task);
+todoList.AddTask(task2);
+todoList.ShowTasks();
--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -13,5 +13,122 @@ namespace tdd_todo_list.CSharp.Test
             TodoList core = new TodoList();
             Assert.Pass();
         }
+
+        [Test]
+        public void TestTaskIsAdded()
+        {
+            Task task = new Task("Do groceries");
+            TodoList todoList = new TodoList();
+            todoList.AddTask(task);
+            Assert.IsTrue(todoList.tasks.Contains(task));
+        }
+
+        [Test]
+        public void TestShowTask()
+        {
+            Task task = new Task("Do groceries");
+            TodoList todoList = new TodoList();
+            todoList.AddTask(task);
+            Assert.AreEqual("Do groceries - Incomplete", task.ToString());
+        }
+
+        [Test]
+        public void TestStatusDefaultIsFalse()
+        {
+            Task task = new Task("Do groceries");
+            TodoList todoList = new TodoList();
+            todoList.AddTask(task);
+            Assert.IsFalse(task.isComplete);
+        }
+
+        [Test]
+        public void TestChangeStatus()
+        {
+            Task task = new Task("Do groceries");
+            TodoList todoList = new TodoList();
+            todoList.AddTask(task);
+            todoList.ChangeStatus(task);
+            Assert.IsTrue(task.isComplete);
+        }
+
+        [Test]
+        public void TestFilterCompleted()
+        {
+            TodoList todoList = new TodoList();
+            Task task = new Task("Do groceries", true);
+            Task task2 = new Task("Clean", false);
+            todoList.AddTask(task);
+            todoList.AddTask(task2);
+            Assert.IsTrue(todoList.GetComplete().Contains(task) && !todoList.GetComplete().Contains(task2));
+        }
+
+        [Test]
+        public void TestFilterIncomplete()
+        {
+            TodoList todoList = new TodoList();
+            Task task = new Task("Do groceries", true);
+            Task task2 = new Task("Clean", false);
+            todoList.AddTask(task);
+            todoList.AddTask(task2);
+            Assert.IsTrue(todoList.GetIncomplete().Contains(task2) && !todoList.GetIncomplete().Contains(task));
+        }
+
+        [Test]
+        public void TestSearchTask()
+        {
+            Task task = new Task("Do groceries");
+            TodoList todoList = new TodoList();
+            todoList.AddTask(task);
+            Assert.AreEqual("Do groceries", todoList.SearchTask(task));
+        }
+
+        [Test]
+        public void TestSearchFail()
+        {
+            Task task = new Task("Do groceries");
+            TodoList todoList = new TodoList();
+            Assert.AreEqual("The task was not found", todoList.SearchTask(task));
+        }
+
+        [Test]
+        public void TestRemoveTask()
+        {
+            Task task = new Task("Do groceries");
+            TodoList todoList = new TodoList();
+            todoList.AddTask(task);
+            todoList.RemoveTask(task);
+            Assert.IsFalse(todoList.tasks.Contains(task));
+        }
+
+        [Test]
+        public void TestRemoveFail()
+        {
+            Task task = new Task("Do groceries");
+            TodoList todoList = new TodoList();
+            todoList.RemoveTask(task);
+            Assert.AreEqual("The task was not found", todoList.RemoveTask(task));
+        }
+
+        [Test]
+        public void TestSortAsc()
+        {
+            TodoList todoList = new TodoList();
+            Task task = new Task("Z");
+            Task task2 = new Task("A");
+            todoList.AddTask(task);
+            todoList.AddTask(task2);
+            Assert.AreEqual("A", todoList.SortAsc()[0].name);
+        }
+
+        [Test]
+        public void TestSortDesc()
+        {
+            TodoList todoList = new TodoList();
+            Task task = new Task("A");
+            Task task2 = new Task("Z");
+            todoList.AddTask(task);
+            todoList.AddTask(task2);
+            Assert.AreEqual("Z", todoList.SortDesc()[0].name);
+        }
     }
 }
