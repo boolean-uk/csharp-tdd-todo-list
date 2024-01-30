094b677567cb44dd6b01ffbffb4a813b5e4737f7
Rose van der Giessen <Rose van der Giessen>
2024-01-30 16:05:17 +0100

    Extension complete - no test refactor

--- tdd-todo-list.CSharp.Main/Program.cs
+++ tdd-todo-list.CSharp.Main/Program.cs
@@ -3,10 +3,4 @@ using tdd_todo_list;
 using tdd_todo_list.CSharp.Main;
 
 Console.WriteLine("Hello, World!");
-tdd_todo_list.Task task = new tdd_todo_list.Task("Do groceries");
-tdd_todo_list.Task task2 = new tdd_todo_list.Task("Clean");
 
-TodoList todoList = new TodoList();
-todoList.AddTask(task);
-todoList.AddTask(task2);
-todoList.ShowTasks();
