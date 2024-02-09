e2d874e60037726972e9c9cd9db19f171103a19a
Elias <elias.experis@se.experis.com>
2024-02-09 12:01:18 +0100

    extension completed

--- domain-model.md
+++ domain-model.md
@@ -1,18 +1,44 @@
+EXTENSION
+Class TodoTask
+properties:
+    private int id
+    string name
+    bool isCompleted
+Methods:
+    void SetName(string name)
+
+    void ToggleStatus()
+
+Class TodoList
+properties:
+    List <TodoTask> tasks
+
+methods:
+TodoTask AddTask(string name)
+    add name to new Task and set isCompleted to false, return true if task doesnt already exist, false if if it already exists. Add to tasks
+TodoTask GetTask(int id)
+    returns TodoTask if id exists
+TodoTask UpdateTaskName(int id, string name)
+    return updated task
+TodoTask ToggleTaskStatus(int id)
+    return updated task
+CORE
 I want to add tasks to my todo list.
 I want to see all the tasks in my todo list.
 I want to change the status of a task between incomplete and complete.
 
 Class TodoTask
+    private int id
     string name
     bool isCompleted
 
 Class TodoList
-PROPETIES: 
-    private List<TodoTask> tasks
+PROPETIES:
+    List<TodoTask> tasks
 
 Methods:
-    bool AddTask(string name)
-        add name to new Task and set isCompleted to false, return true if task doesnt already exist, false if if it already exists
+    TodoTask AddTask(string name)
+        add name to new Task and set isCompleted to false, return TodoTask if task doesnt already exist, null if if it already exists
     bool RemoveTask(string name)
         check if task exists, remove from tasks, return new List<TodoTask>
     bool ToggleComplete(string name)
@@ -28,4 +54,9 @@ Methods:
     List <TodoTask> SortedTasksAsc()
         iterate through tasks and return a new List<TodoTask> sorted by name ascending
     List <TodoTask> SortedTasksDesc()
-        iterate through tasks and return a new List<TodoTask> sorted by name descending      
+        iterate through tasks and return a new List<TodoTask> sorted by name descending
+
+    I want to be able to get a task by a unique ID.
+    I want to update the name of a task by providing its ID and a new name.
+    I want to be able to change the status of a task by providing its ID.
+    I want to be able to see the date and time that I created each task.
--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -3,10 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework.Interfaces;
 
-namespace tdd_todo_list.CSharp.Main
+namespace tdd_todo_list.CSharp.Extension
 {
     public class TodoListExtension
     {
+        public class TodoList
+        {
+            public List<TodoTask> Tasks { get; }
+            public TodoList()
+            {
+                Tasks = new List<TodoTask>();
+            }
+
+            public TodoTask AddTask(string name)
+            {
+                TodoTask task = new TodoTask(name);
+                Tasks.Add(task);
+                return task;
+            }
+
+            public TodoTask GetTask(int id)
+            {
+                TodoTask? result = Tasks.Where(t => t.Id == id).SingleOrDefault();
+                return result;
+            }
+
+            public TodoTask UpdateTaskName(int id, string name)
+            {
+                TodoTask task = GetTask(id);
+                if (task == null)
+                {
+                    return null;
+                }
+                
+                if (name != null)
+                {
+                    task.SetName(name);
+                }
+                return task;
+            }
+
+            public TodoTask ToggleTaskStatus(int id)
+            {
+                TodoTask task = GetTask(id);
+                if (task == null)
+                {
+                    return null;
+                }
+
+                task.ToggleStatus();
+
+
+                return task;
+            }
+        }
+        public class TodoTask
+        {
+            public int Id { get; }
+            public string Name { get; set; }
+            public bool IsCompleted { get; set; }
+            public DateTime Time { get; }
+
+            public TodoTask(string name)
+            {
+                this.Id = Guid.NewGuid().GetHashCode();
+                this.Name = name;
+                this.IsCompleted = false;
+                this.Time = DateTime.Now;
+            }
+
+            public void ToggleStatus()
+            {
+                this.IsCompleted = !this.IsCompleted;
+            }
+
+            public void SetName(string name)
+            {
+                this.Name = name;
+            }
+
+
+        }
     }
 }
