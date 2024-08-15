91a4b9f491915b505c648fcdc7df4931565f26ec
John Ferdie Abueg <john.abueg@gmail.com>
2024-08-15 10:28:34 +0200

    Skeleton tests

--- tdd-todo-list.CSharp.Test/ToDoListTests.cs
+++ tdd-todo-list.CSharp.Test/ToDoListTests.cs
@@ -19,7 +19,84 @@ namespace tdd_todo_list.CSharp.Test
         }
         
         [Test]
-        public void GetTasksInTODOList()
+        public void GetTasksFromTODOList()
+        {
+            // 1. Setup
+            TodoList todoList = new TodoList();
+            // 2. Execute
+
+            // 3. Verify
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void ChangeTaskOfStatus()
+        {
+            // 1. Setup
+            TodoList todoList = new TodoList();
+            // 2. Execute
+
+            // 3. Verify
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void GetCompletedTasksFromTODOList()
+        {
+            // 1. Setup
+            TodoList todoList = new TodoList();
+            // 2. Execute
+
+            // 3. Verify
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void GetIncompleteTasksFromTODOList()
+        {
+            // 1. Setup
+            TodoList todoList = new TodoList();
+            // 2. Execute
+
+            // 3. Verify
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void SearchForTaskInTODOList()
+        {
+            // 1. Setup
+            TodoList todoList = new TodoList();
+            // 2. Execute
+
+            // 3. Verify
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void RemoveTaskFromTODOList()
+        {
+            // 1. Setup
+            TodoList todoList = new TodoList();
+            // 2. Execute
+
+            // 3. Verify
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void GetListInAscendingOrderFromTODOList()
+        {
+            // 1. Setup
+            TodoList todoList = new TodoList();
+            // 2. Execute
+
+            // 3. Verify
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void GetListInDescendingOrderFromTODOList()
         {
             // 1. Setup
             TodoList todoList = new TodoList();
