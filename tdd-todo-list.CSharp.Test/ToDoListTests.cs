8fbd7d898c4478b4dcb8e28ee648bbf17dd95b6e
John Ferdie Abueg <john.abueg@gmail.com>
2024-08-15 10:10:23 +0200

    Renamed CoreTests to ToDoListTests and started setting up tests

---
+++ tdd-todo-list.CSharp.Test/ToDoListTests.cs
@@ -0,0 +1,20 @@
+﻿using tdd_todo_list.CSharp.Main;
+using NUnit.Framework;
+
+namespace tdd_todo_list.CSharp.Test
+{
+    [TestFixture]
+    public class ToDoListTests
+    {
+
+        [Test]
+        public void AddTaskToTODOList()
+        {
+            // 1. Setup
+            // 2. Execute
+            TodoList todoList = new TodoList();
+            // 3. Verify
+            Assert.Fail();
+        }
+    }
+}
