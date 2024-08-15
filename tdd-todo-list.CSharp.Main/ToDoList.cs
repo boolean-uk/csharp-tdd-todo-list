5f2dc8eb93b57961ac9958606a8f7d8cf01d13a8
Victor <victor.chicinas@gmail.com>
2024-08-15 08:55:07 +0200

    CoreTests and functions defindes empty

--- tdd-todo-list.CSharp.Main/ToDoList.cs
+++ tdd-todo-list.CSharp.Main/ToDoList.cs
@@ -8,5 +8,54 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoList
     {
+        public void AddTaskToList(string task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTaskToList(object task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getCompleteStasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getIncompleteStasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getInCompleteTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getIndividualTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getOrderedTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getOrderedTasksReversed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeTask()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -8,10 +8,85 @@ namespace tdd_todo_list.CSharp.Test
     {
 
         [Test]
-        public void FirstTest()
+        public void addTaskToList()
         {
             TodoList core = new TodoList();
-            Assert.Pass();
+            Task task = new Task("First task", "incomplete");
+            core.AddTaskToList(task);
+            Assert.Fail();
         }
+
+        [Test]
+        public void seeAllTasktInList()
+        {
+            TodoList core = new TodoList();
+            Task task = new Task("First task", "incomplete");
+            core.AddTaskToList(task);
+            core.ToString();
+            Assert.Fail();
+        }
+
+        [Test]
+        public void changeStatusOfTask()
+        {
+            TodoList core = new TodoList();
+            Task task = new Task("First task", "incomplete");
+            core.AddTaskToList(task);
+            core.ChangeStatus();
+            Assert.Fail();
+        }
+        [Test]
+        public void getCompleteTasks()
+        {
+            TodoList core = new TodoList();
+            
+            core.getCompleteStasks();
+            Assert.Fail();
+        }
+
+        public void getInCompleteTasks()
+        {
+            TodoList core = new TodoList();
+
+            core.getInCompleteTasks();
+            Assert.Fail();
+        }
+
+        public void getIndividualTask()
+        {
+            TodoList core = new TodoList();
+
+            core.getIndividualTasks();
+            Assert.Fail();
+        }
+
+
+        public void removeTask()
+        {
+            TodoList core = new TodoList();
+
+            core.removeTask();
+            Assert.Fail();
+        }
+
+        public void orderedTasksAlphabeticaly()
+        {
+            TodoList core = new TodoList();
+
+            core.getOrderedTasks();
+            Assert.Fail();
+        }
+
+        public void orderedTasksReversedAlphabeticaly()
+        {
+            TodoList core = new TodoList();
+
+            core.getOrderedTasksReversed();
+            Assert.Fail();
+        }
+
+
+
+
     }
 }
