55d7f84a2f6a268c8cf9ae45871bdf234a89980a
Victor <victor.chicinas@gmail.com>
2024-08-15 12:29:21 +0200

    Status test done

--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -22,7 +22,29 @@ namespace tdd_todo_list.CSharp.Test
             Assert.That(core.getIndividualTasks(id).Equals(task));
         }
 
+        [Test]
+        public void uppdateTaskNameByUniqueID()
+        {
+            TodoList core = new TodoList();
+            TodoTaskObj task = new TodoTaskObj("First task", false);
+            int id = task.Id;
+            core.AddTaskToList(task);
+            core.getIndividualTasks(id).ContentText = "Hej";
+            Assert.That(core.getIndividualTasks(id).ContentText.Equals("Hej"));
+        }
 
-        
+        [Test]
+        public void uppdateTaskStatusByUniqueID()
+        {
+            TodoList core = new TodoList();
+            TodoTaskObj task = new TodoTaskObj("First task", false);
+            int id = task.Id;
+            core.AddTaskToList(task);
+            core.getIndividualTasks(id).ChangeStatus();
+            Assert.True(core.getIndividualTasks(id).Complete);
+        }
+
+
+
     }
 }
