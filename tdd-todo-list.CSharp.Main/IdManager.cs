c45daa8447a2f69f9de3870a2e35f047e7a048f3
Felix <felix.mathiasson99@gmail.com>
2024-08-15 12:25:35 +0200

    source code for findByID written and all tests passed

--- tdd-todo-list.CSharp.Main/IdManager.cs
+++ tdd-todo-list.CSharp.Main/IdManager.cs
@@ -8,11 +8,11 @@ namespace tdd_todo_list.CSharp.Main
 {// THIS IS EXTENSION
     public class IdManager
     {
-        struct TaskInfo(string name, bool status, string timeStamp)
+        public struct TaskInfo(string name, bool status, string timeStamp)
         {
-            string name = name;
-            bool status = status;
-            string timeStamp = timeStamp;   
+            public string name = name;
+            public bool status = status;
+            public string timeStamp = timeStamp;   
         }
         Dictionary<int, TaskInfo> todo = new Dictionary<int, TaskInfo>();
         public enum TodoTaskStatus
@@ -22,5 +22,33 @@ namespace tdd_todo_list.CSharp.Main
             All
         }
 
+        public void AddTask(string name, bool status, string timeStamp)
+        {
+            TaskInfo info = new TaskInfo(name, status, timeStamp);
+            int id = todo.Count;
+            todo.Add(id, info);
+        }
+
+        public string FindTaskByID(int id)
+        {
+            string resultInfo = "404 task not found!";
+            foreach (var entry in todo)
+            {
+                if (entry.Key == id)
+                {
+                    var temp = entry.Value;
+                    string completion = ", task incomplete";
+                    if(temp.status)
+                    {
+                        completion = ", task complete";
+                    }
+                    resultInfo = temp.name + completion + ", task created " + temp.timeStamp;
+                    break;
+                }
+            }
+           
+
+            return resultInfo;
+        }
     }
 }
