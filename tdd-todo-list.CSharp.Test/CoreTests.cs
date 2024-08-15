8a126f1e8f94b52c6c4af3000263d989ee12788a
Julia <julia.r.lindgren@gmail.com>
2024-08-15 10:23:08 +0200

    Create status tests

--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -18,6 +18,27 @@ namespace tdd_todo_list.CSharp.Test
             Assert.IsTrue(todoList.Tasks.Count() == 1);
         }
 
+        [Test]
+        public void CheckStatusFalse()
+        {
+            TodoList todoList = new TodoList();
+
+            todoList.AddTask("Hoover");
+
+            Assert.IsFalse(todoList.Tasks[0].IsComplete);
+        }
+
+        [Test]
+        public void CheckStatusTrue()
+        {
+            TodoList todoList = new TodoList();
+
+            todoList.AddTask("Hoover");
+            todoList.Tasks[0].IsComplete = true;
+
+            Assert.IsTrue(todoList.Tasks[0].IsComplete);
+        }
+
 
     }
 }
