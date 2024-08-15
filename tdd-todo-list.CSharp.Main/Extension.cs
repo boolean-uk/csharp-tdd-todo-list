bcfecec8422e9a7a9deaff524087d970f5803120
tvaltn <valtniemi@gmail.com>
2024-08-15 13:58:52 +0200

    started on extension...

--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -8,5 +8,27 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoListExtension
     {
+        // Could use another dictionary within the dictionary, but that sounds like extra work!
+        // so we're using a tuple instead
+        Dictionary<int, (string, bool, DateTime)> _todoList;
+        public TodoListExtension()
+        {
+            _todoList = new Dictionary<int, (string, bool, DateTime)>();
+        }
+
+        public void add(int id, string task, bool status, DateTime dateTime)
+        {
+            _todoList.Add(id, (task, status, dateTime));
+        }
+
+        public (string, bool, DateTime) getTask(int id)
+        {
+            return _todoList[id];
+        }
+
+        public void updateTask(int id, string v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -4,15 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace tdd_todo_list.CSharp.Test
 {
     public class ExtensionTests
     {
-        private TodoListExtension _extension;
-        public ExtensionTests()
+        [Test]
+        public void ETest1()
         {
-            _extension = new TodoListExtension();
+            var extension = new TodoListExtension();
+            int id = 0;
+            string task = "Walk the dog";
+            bool status = false;
+            DateTime dateTime = DateTime.Now;
+            extension.add(id, task, status, dateTime);
+            (string, bool, DateTime) result = extension.getTask(id);
+            Assert.That(result.Item1.Equals(task));
+        }
+
+        [Test]
+        public void ETest2()
+        {
+            var extension = new TodoListExtension();
+            int id = 0;
+            string task = "Walk the dog";
+            bool status = false;
+            DateTime dateTime = DateTime.Now;
+            extension.add(id, task, status, dateTime);
+            extension.updateTask(id, "Pet the dog");
+
+            Assert.That(extension.getTask(id).Item1.Equals("Pet the dog"));
+
         }
     }
 }
