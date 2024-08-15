b5a78a585ad9d4490426f175ecceaac936aeff38
Jonas <jonashalvorsen2013@live.no>
2024-08-15 15:04:22 +0200

    Exercise-created-extension-part

--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -8,5 +8,107 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoListExtension
     {
+        int IDCounter = 0;
+        private List<JobExtension> _jobs = new List<JobExtension>();
+        public List<JobExtension> Jobs { get { return _jobs; } }
+
+        public bool AddToList(JobExtension job)
+        {
+            if (job == null)
+            {
+                return false;
+            }
+            else if (_jobs.Where(item => item.Name == job.Name).ToList().Count != 0)
+            {
+                return false;
+            }
+            IDCounter++;
+            job.ID = IDCounter;
+            _jobs.Add(job);
+            return true;
+        }
+
+        public JobExtension GetJob(string name)
+        {
+            List<JobExtension> jobs = Jobs.Where(job => job.Name.Equals(name)).ToList();
+            if (jobs.Count <= 0)
+            {
+                return new JobExtension("", "");
+            }
+            return jobs[0];
+        }
+
+        public List<JobExtension> GetJobs(bool completed)
+        {
+            return Jobs.Where(job => job.Complete.Equals(completed)).ToList();
+        }
+
+        public bool PrintJobs(List<JobExtension> jobs)
+        {
+            string message = string.Empty;
+            bool messageComplete = false;
+
+            foreach (var item in jobs)
+            {
+                message += string.Format($"Name: {item.Name}\n");
+                message += string.Format("Status: {0}", item.Complete ? "Complete" : "Incomplete\n");
+                message += string.Format($"Description: {item.Description}\n");
+                message += string.Format($"Created: {item.Created}\n\n");
+
+                messageComplete = message.Contains(item.Name);
+                if (messageComplete.Equals(false))
+                {
+                    return false;
+                }
+            }
+            Console.WriteLine(message);
+
+            return messageComplete;
+        }
+
+        public bool RemoveJob(string name)
+        {
+            return Jobs.Remove(GetJob(name));
+        }
+
+        public List<JobExtension> GetJobsOrdered(bool ascending)
+        {
+            if (ascending)
+            {
+                return Jobs.OrderBy(job => job.Name).ToList();
+            }
+            return Jobs.OrderByDescending(job => job.Name).ToList();
+        }
+    }
+
+    public class JobExtension
+    {
+        private bool _complete;
+        private string _name;
+        private string _description;
+        private DateTime _created;
+        private int _ID;
+
+        public int ID { get; set; }
+        public bool Complete { get { return _complete; } }
+        public string Name { get { return _name; } }
+        public string Description { get { return _description; } }
+        public string Created { get { return DateTime.Now.ToString(); } }
+
+        public JobExtension(string name, string description)
+        {
+            _complete = false;
+            _name = name;
+            _description = description;
+            _created = DateTime.Now;
+        }
+
+        public bool ChangeStatus(bool newStatus)
+        {
+            this._complete = newStatus;
+            return true;
+        }
     }
 }
+
+
--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -4,15 +4,163 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace tdd_todo_list.CSharp.Test
 {
+    [TestFixture]
     public class ExtensionTests
     {
         private TodoListExtension _extension;
         public ExtensionTests()
         {
             _extension = new TodoListExtension();
+        }
+
+        [TestCase("Do the dishes", "The dishes shall be clean, but i dont like doing the work")]
+        public void TestAddJob(string name, string description)
+        {
+            TodoListExtension todoListExtension = new TodoListExtension();
+            JobExtension job = new JobExtension(name, description);
+            bool addedExpected = true;
+
+            bool addedResult = todoListExtension.AddToList(job);
+
+            Assert.That(addedResult, Is.EqualTo(addedExpected));
+        }
+
+        [TestCase("Do the dishes", "The dishes shall be clean, but i dont like doing the work")]
+        [TestCase("Go for a run", "Run 1 km, its hard, but its good for you!")]
+        public void TestPrintJobs(string name, string description)
+        {
+            TodoListExtension todoListExtension = new TodoListExtension();
+            JobExtension job1 = new JobExtension(name, description);
+            JobExtension job2 = new JobExtension(name, description);
+            todoListExtension.AddToList(job1);
+            todoListExtension.AddToList(job2);
+
+            bool printJobsExpected = true;
+
+            bool printJobsResult = todoListExtension.PrintJobs(todoListExtension.Jobs);
+
+            Assert.That(printJobsResult, Is.EqualTo(printJobsExpected));
+        }
+
+        [TestCase(true, "Do the dishes", "The dishes shall be clean, but i dont like doing the work")]
+        public void TestChangeStatus(bool newStatus, string name, string description)
+        {
+            JobExtension job1 = new JobExtension(name, description);
+            bool statusChangedExpected = true;
+
+            bool statusChangedResult = job1.ChangeStatus(newStatus);
+            Assert.That(statusChangedResult, Is.EqualTo(statusChangedExpected));
+
+            newStatus = false;
+            statusChangedResult = job1.ChangeStatus(newStatus);
+            Assert.That(statusChangedResult, Is.EqualTo(statusChangedExpected));
+        }
+
+        [Test]
+        public void TestGetCompleteJobs()
+        {
+            TodoListExtension todoListExtension = new TodoListExtension();
+            JobExtension job1 = new JobExtension("Do the dishes", "The dishes shall be clean, but i dont like doing the work");
+            JobExtension job2 = new JobExtension("Go for a run", "Run 1 km, its hard, but its good for you!");
+            todoListExtension.AddToList(job1);
+            job2.ChangeStatus(true);
+            todoListExtension.AddToList(job2);
+
+            List<JobExtension> expectedJobList = new List<JobExtension>() { job2 };
+
+            List<JobExtension> actualJobList = todoListExtension.GetJobs(true);
+
+            Assert.That(actualJobList, Is.EquivalentTo(expectedJobList));
+        }
+
+        [Test]
+        public void TestGetIncompleteJobs()
+        {
+            TodoListExtension todoListExtension = new TodoListExtension();
+            JobExtension job1 = new JobExtension("Do the dishes", "The dishes shall be clean, but i dont like doing the work");
+            JobExtension job2 = new JobExtension("Go for a run", "Run 1 km, its hard, but its good for you!");
+            todoListExtension.AddToList(job1);
+            job2.ChangeStatus(true);
+            todoListExtension.AddToList(job2);
+
+            List<JobExtension> expectedJobList = new List<JobExtension>() { job1 };
+
+            List<JobExtension> actualJobList = todoListExtension.GetJobs(false);
+
+            Assert.That(actualJobList, Is.EquivalentTo(expectedJobList));
+        }
+
+        [Test]
+        public void TestGetJob()
+        {
+            TodoListExtension todoListExtension = new TodoListExtension();
+            JobExtension job1 = new JobExtension("Do the dishes", "The dishes shall be clean, but i dont like doing the work");
+            JobExtension job2 = new JobExtension("Go for a run", "Run 1 km, its hard, but its good for you!");
+            todoListExtension.AddToList(job1);
+            todoListExtension.AddToList(job2);
+
+            JobExtension expectedJob = job1;
+
+            JobExtension actualJob = todoListExtension.GetJob("Do the dishes");
+
+            Assert.That(actualJob, Is.EqualTo(expectedJob));
+        }
+
+        [Test]
+        public void TestRemoveJob()
+        {
+            TodoListExtension todoListExtension = new TodoListExtension();
+            JobExtension job1 = new JobExtension("Do the dishes", "The dishes shall be clean, but i dont like doing the work");
+            JobExtension job2 = new JobExtension("Go for a run", "Run 1 km, its hard, but its good for you!");
+            todoListExtension.AddToList(job1);
+            todoListExtension.AddToList(job2);
+
+            bool expectedResult = true;
+
+            bool actualResult = todoListExtension.RemoveJob("Do the dishes");
+
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+
+            expectedResult = false;
+
+            actualResult = todoListExtension.RemoveJob("Do the dishes");
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void TestGetJobsOrderedAscending()
+        {
+            TodoListExtension todoListExtension = new TodoListExtension();
+            JobExtension job1 = new JobExtension("Go for a run", "Run 1 km, its hard, but its good for you!");
+            JobExtension job2 = new JobExtension("Do the dishes", "The dishes shall be clean, but i dont like doing the work");
+            todoListExtension.AddToList(job1);
+            todoListExtension.AddToList(job2);
+
+            List<JobExtension> expectedResult = new List<JobExtension>() { job1, job2 };
+
+            List<JobExtension> actualResult = todoListExtension.GetJobsOrdered(true);
+
+            Assert.That(actualResult, Is.EquivalentTo(expectedResult));
+        }
+
+        [Test]
+        public void TestGetJobsOrderedDescending()
+        {
+            TodoListExtension todoListExtension = new TodoListExtension();
+            JobExtension job1 = new JobExtension("Go for a run", "Run 1 km, its hard, but its good for you!");
+            JobExtension job2 = new JobExtension("Do the dishes", "The dishes shall be clean, but i dont like doing the work");
+            todoListExtension.AddToList(job1);
+            todoListExtension.AddToList(job2);
+
+            List<JobExtension> expectedResult = new List<JobExtension>() { job2, job1 };
+
+            List<JobExtension> actualResult = todoListExtension.GetJobsOrdered(false);
+
+            Assert.That(actualResult, Is.EquivalentTo(expectedResult));
         }
     }
 }
