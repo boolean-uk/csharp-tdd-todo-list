2980f9cab0e375e2ec6111e50e6c448d308e4e3c
GRehan <GRehan@N-NO-01-01-3380>
2025-01-09 11:27:53 +0100

    Wrote add task test and code

--- tdd-todo-list.CSharp.Main/ToDoList.cs
+++ tdd-todo-list.CSharp.Main/ToDoList.cs
@@ -8,5 +8,15 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoList
     {
+        public List<Task> todolist = new List<Task>();
+
+
+        public string Add(string Task)
+        {
+            Task task = new Task(Task);
+            todolist.Add(task);
+            return task.ToString();
+        }
+
     }
 }
--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -1,5 +1,6 @@
 ﻿using tdd_todo_list.CSharp.Main;
 using NUnit.Framework;
+using Task = tdd_todo_list.CSharp.Main.Task;
 
 namespace tdd_todo_list.CSharp.Test
 {
@@ -8,10 +9,20 @@ namespace tdd_todo_list.CSharp.Test
     {
 
         [Test]
-        public void FirstTest()
+        public void AddTaskToList()
         {
+            var task = "Clean your room";
             TodoList core = new TodoList();
-            Assert.Pass();
+            core.Add(task);
+            Assert.That(core.todolist.Count(), Is.EqualTo(1));
+        }
+        [Test]
+        public void AddTaskToList2()
+        {
+            var task = "Clean your room";
+            TodoList core = new TodoList();
+            core.Add(task);
+            Assert.That(core.todolist[0].task, Is.EqualTo("Clean your room"));
         }
     }
 }
