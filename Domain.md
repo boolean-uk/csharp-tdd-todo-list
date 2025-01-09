65e76dca38d29ea2abad265accfb695ea592c57b
Sander Rasmussen <sander.rasmussn@gmail.com>
2025-01-09 14:09:34 +0100

    solved part 1

---
+++ Domain.md
@@ -0,0 +1,12 @@
+| Classes       |  Methods/Properties | Scenario       | Outputs  |
+|:-----------	|:-------------------:|:-----------------:|-------------:|
+| TodoList.cs   | AddTask(Task task)  | adds task to list  |  |
+| TodoList.cs   |  GetAllTasks()  |   returns all tasks | list containing all task objects |
+| TodoList.cs   | Completed()   |  changes task status to completed   |  | 
+| TodoList.cs   | Uncompleted()   |  changes task status to completed   |  | 
+|  TodoList.cs |  |  GetCompletedTasks()   |  gets list of completed items | returns a lsit of completed items| 
+| TodoList.cs | GetIncompletedTasks()| gets a list of uncompleted tasks    |  returns list of uncompleted tasks| 
+| TodoList.cs |SearchTask(string task) | searches for a task    | returns task found if exists, else receive a message saying task wasnt found  | 
+| TodoList.cs  | RemoveTask(Task task) | removes task form list    |   | 
+| TodoList.cs | GetAllTasksAscending() | gets a list with all tasks ordered ascending alphabeticaly    | returns list | 
+| TodoList.cs | GetAllTasksDescening() |  gets list with all tasks ordered descending     |  returns list|
--- tdd-todo-list.CSharp.Main/ToDoList.cs
+++ tdd-todo-list.CSharp.Main/ToDoList.cs
@@ -3,10 +3,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace tdd_todo_list.CSharp.Main
 {
     public class TodoList
     {
+        List<Task> tasks = new List<Task>();
+
+        public int ListLength()
+        {
+            return tasks.Count;
+        }
+        public List<Task> GetAllTasks()
+        {
+            return tasks;
+        }
+
+        public void AddTask(string description)
+        {
+            Task newTask = new Task();
+            newTask.description = description;
+            newTask.completed = false;
+            tasks.Add(newTask);
+        }
+        public void Completed(Task task)
+        {
+            task.completed = true;
+        }
+        public void Uncompleted(Task task)
+        {
+            task.completed = false;
+        }
+        public List<Task> GetCompletedTasks()
+        {
+            List<Task> result = new List<Task>();
+            foreach (Task task in tasks)
+            {
+                if (task.completed)
+                {
+                    result.Add(task);
+                }
+            }
+            return result;
+        }
+        public List<Task> GetIncompletedTasks()
+        {
+            List<Task> result = new List<Task>();
+            foreach (Task task in tasks)
+            {
+                if (!task.completed)
+                {
+                    result.Add(task);
+                }
+            }
+            return result;
+        }
+        //returns a Task objekt if task was found, else return a string message
+        public Task SearchTask(string description)
+        {
+            foreach (Task task in tasks)
+            {
+                if (task.description == description)
+                {
+                    return task;
+                }
+            }
+            Console.WriteLine("the requested task was not found in the ToDo list");
+            return new Task(); //returns empty task if no task found
+        }
+        //returns true if removal succesfull, else false
+        public bool RemoveTask(Task task)
+        {
+            Object? toRemove = tasks.Where(x => x.Equals(task));
+            if (toRemove != null)
+            {
+                for (int i = 0; i < tasks.Count; i++)
+                {
+                    if (tasks[i].Equals(task))
+                    {
+                        tasks.RemoveAt(i);
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+        public List<Task> GetAllTasksAscending()
+        {
+            List<Task> sorted = tasks.OrderBy(x => x.description).ToList();
+            return sorted;
+        }
+        public List<Task> GetAllTasksDescening()
+        {
+            List<Task> sorted = tasks.OrderBy(x => x.description).ToList();
+            sorted.Reverse();   
+            return sorted;
+        }
     }
 }
