d2c8ef909e5c05cf76dcfb605c9b35c9a9af2f3e
Sander Rasmussen <sander.rasmussn@gmail.com>
2025-01-09 15:37:52 +0100

    Solved extension

--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -4,15 +4,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+using System.ComponentModel;
+using static tdd_todo_list.CSharp.Main.TodoListExtension;
+using System.Security.Cryptography;
 
 namespace tdd_todo_list.CSharp.Test
 {
+    [TestFixture]
     public class ExtensionTests
     {
         private TodoListExtension _extension;
         public ExtensionTests()
         {
             _extension = new TodoListExtension();
+        }
+        [Test]
+        public void GetTask()
+        {
+            _extension.AddTask("do something");
+            _extension.AddTask("buy food");
+            ExtensionTask task = _extension.GetTask(1);
+            Assert.That(task.description, Is.EqualTo("buy food"));
+
+
+        }
+        [Test]
+        public void UpdateTask()
+        {
+            _extension.AddTask("do something");
+            _extension.AddTask("buy food");
+
+            _extension.UpdateTask(1, "find a new task");
+            ExtensionTask task = _extension.GetTask(1);
+            Assert.That(task.description, Is.EqualTo("find a new task"));
+        }
+        [Test]
+        public void UpdateCompleted()
+        {
+            _extension.AddTask("do something");
+            _extension.AddTask("buy food");
+            _extension.UpdateCompleted(1, true);
+            ExtensionTask? task = _extension.GetTask(1);
+            Assert.That(task.completed, Is.EqualTo(true));
+        }
+        [Test]
+        public void TimeCreated()
+        {   
+            DateTime startTime = DateTime.Now;
+            Thread.Sleep(1000);
+            _extension.AddTask("do something");
+            _extension.AddTask("buy food");
+            DateTime endtime = DateTime.Now;
+            Assert.That(_extension.GetTask(0).time <= startTime && _extension.GetTask(0).time <= endtime) ;
         }
     }
 }
