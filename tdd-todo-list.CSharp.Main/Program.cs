a23b5139bab0bdcf13ac17c3c67add2f1c30f980
stp <steventphung6@gmail.com>
2025-01-09 15:04:51 +0100

    Steven Phung

--- tdd-todo-list.CSharp.Main/Program.cs
+++ tdd-todo-list.CSharp.Main/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using tdd_todo_list.CSharp.Main;
+
 Console.WriteLine("Hello, World!");
+
+TodoList todoList = new TodoList();
+
+Console.WriteLine(todoList.viewTasks());
+Console.WriteLine("---------");
+Console.WriteLine(todoList.viewCompletedTasks());
+Console.WriteLine(todoList.viewIncompletedTasks());
+Console.WriteLine(todoList.UpdateTask("Dishes"));
+Console.WriteLine(todoList.UpdateTask("Laundry"));
+Console.WriteLine(todoList.RemoveTask("Grocery Shopping"));
+Console.WriteLine("---------");
+
+Console.WriteLine(todoList.viewTasks());
+
+Console.WriteLine(todoList.SearchTask("Laundry"));
+
+Console.WriteLine("---------");
+
+Console.WriteLine(todoList.SortAscTask());
+Console.WriteLine(todoList.SortDescTask());
+
+
+
