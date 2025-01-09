a64d0fe400180fa04387cba82f46f1fc8c75bffa
GRehan <GRehan@N-NO-01-01-3380>
2025-01-09 11:43:10 +0100

    Wrote test for view task

--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -24,5 +24,29 @@ namespace tdd_todo_list.CSharp.Test
             core.Add(task);
             Assert.That(core.todolist[0].task, Is.EqualTo("Clean your room"));
         }
+
+        [Test]
+        public void ViewTasks()
+        {
+            var tasks = new List<string>
+            {
+                "Clean your room",
+                "Do the laundry",
+                "Buy groceries",
+                "Prepare dinner",
+                "Read a book"
+            };
+            TodoList core = new TodoList();
+
+            foreach (var task in tasks)
+            {
+                core.Add(task);
+            }
+
+            var viewTask = core.View();
+            var expected = tasks;
+
+            Assert.That(expected, Is.EqualTo(viewTask));
+        }
     }
 }
