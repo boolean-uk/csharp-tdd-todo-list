665b33a40b0fd9f7a3d17e6a465ff884ecdc759a
Erlend Skutlaberg <skutlis98@gmail.com>
2025-01-09 16:03:51 +0100

    completed the tasks

--- tdd-todo-list.CSharp.Main/Program.cs
+++ tdd-todo-list.CSharp.Main/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.ComponentModel;
+using tdd_todo_list.CSharp.Main;
+
+TodoListExtension l = new TodoListExtension();
+l.Add("Task 1", "work");
+l.Add("Task 2", "Coffee");
+l.Add("Task 3", "Lunch");
+l.Add("Task 4", "Go Home");
+
+string time = l.GetCreationTime();
+Console.WriteLine(time);
+
--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace tdd_todo_list.CSharp.Test
 {
@@ -14,5 +15,78 @@ namespace tdd_todo_list.CSharp.Test
         {
             _extension = new TodoListExtension();
         }
+
+        [Test]
+        public void TestGetIds()
+        {
+            _extension = new TodoListExtension();
+            TaskExtension t1 = _extension.Add("1", "");
+            TaskExtension t2 = _extension.Add("2", "");
+            TaskExtension t3 = _extension.Add("3", "");
+
+            List<long> ids = _extension.GetIDs();
+
+            Assert.Contains(t1.id, ids);
+            Assert.Contains(t2.id, ids);
+            Assert.Contains(t3.id, ids);
+
+        }
+
+
+        [TestCase("Task 1", "Do some work")]
+        [TestCase("Task 2", "Take a break")]
+        public void TestGetTaskById(string title, string description)
+        {
+            _extension = new TodoListExtension();
+            TaskExtension t1 = _extension.Add(title, description);
+
+            TaskExtension retrievedTask = _extension.GetTaskByID(t1.id);
+
+            Assert.True(t1.Equals(retrievedTask));
+        }
+
+
+        [TestCase("akdnskd", "...", "ksdskdmks")]
+        [TestCase("ajsndfouasbfd", "...", "saudasodhasjodhasjd")]
+        public void UpdateTaskTitleByID(string title, string description, string new_title)
+        {
+            _extension = new TodoListExtension();
+            TaskExtension t1 = _extension.Add(title, description);
+
+            TaskExtension updated = _extension.UpdateTaskTitle(t1.id, new_title);
+
+            Assert.NotNull(_extension.tasks.FirstOrDefault(x => x.title == new_title));
+            Assert.Null(_extension.tasks.FirstOrDefault(x => x.title == title));
+        }
+
+        [TestCase("Task 1", "Do some work")]
+        [TestCase("Task 2", "Take a break")]
+        public void TestUpdateTaskStatusByID(string title, string description)
+        {
+            _extension = new TodoListExtension();
+            TaskExtension t1 = _extension.Add(title, description);
+            Assert.False(t1.status);
+            TaskExtension updatedTask = _extension.UpdateTaskStatus(t1.id, true);
+
+            Assert.True(updatedTask.status);
+        }
+
+        [TestCase("Task 1", "Do some work")]
+        [TestCase("Task 2", "Take a break")]
+        public void TestTaskListToStr(string title, string description)
+        {
+            _extension = new TodoListExtension();
+            TaskExtension t1 = _extension.Add(title, description);
+            string creationTime = _extension.GetCreationTime();
+            string expectedOutput = t1.id + " " + t1.title + " " + t1.creationTime.ToString() + "\n";
+            Assert.That(expectedOutput, Is.EqualTo(creationTime));
+
+        }
+
+
+
+
+
+
     }
 }
