58c8c31c925458d442dd051dc5c26f978a27c9bd
GRehan <GRehan@N-NO-01-01-3380>
2025-01-09 14:37:39 +0100

    Extension: + TEST ChangeStatusById

--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -35,30 +35,49 @@ namespace tdd_todo_list.CSharp.Test
             var taskByID = extension.GetTaskById(taskId);
             Assert.That(tasks[2], Is.EqualTo(taskByID));
         }
-            [Test]
-            public void UpdateTaskByIdAndName()
+        [Test]
+        public void UpdateTaskByIdAndName()
+        {
+            var task1 = new Task("clean your room", false);
+            var task2 = new Task("do the laundry", false);
+            var task3 = new Task("buy groceries", true);
+            var task4 = new Task("prepare dinner", false);
+            var task5 = new Task("read a book", true);
+            List<Task> tasks = new List<Task> { task1, task2, task3, task4, task5 };
+
+            TodoListExtension extension = new TodoListExtension();
+            foreach (var task in tasks)
             {
-                var task1 = new Task("clean your room", false);
-                var task2 = new Task("do the laundry", false);
-                var task3 = new Task("buy groceries", true);
-                var task4 = new Task("prepare dinner", false);
-                var task5 = new Task("read a book", true);
-                List<Task> tasks = new List<Task> { task1, task2, task3, task4, task5 };
-
-                TodoListExtension extension = new TodoListExtension();
-                foreach (var task in tasks)
-                {
-                    extension.Add(task);
-                }
-
-                int taskId = 3;
-                string newTaskName = "watch a documentary";
-                var updatedTask = extension.UpdateTaskById(taskId, newTaskName);
-
-                Assert.That(updatedTask.task, Is.EqualTo(tasks[2].task));
-                Assert.That(updatedTask.id, Is.EqualTo(tasks[2].id));
-
-
+                extension.Add(task);
             }
+
+            int taskId = 3;
+            string newTaskName = "watch a documentary";
+            var updatedTask = extension.UpdateTaskById(taskId, newTaskName);
+
+            Assert.That(updatedTask.task, Is.EqualTo(tasks[2].task));
+            Assert.That(updatedTask.id, Is.EqualTo(tasks[2].id));
+        }
+        [Test]
+        public void UpdateTaskStatusById()
+        {
+            var task1 = new Task("clean your room", false);
+            var task2 = new Task("do the laundry", false);
+            var task3 = new Task("buy groceries", true);
+            var task4 = new Task("prepare dinner", false);
+            var task5 = new Task("read a book", true);
+            List<Task> tasks = new List<Task> { task1, task2, task3, task4, task5 };
+
+            TodoListExtension extension = new TodoListExtension();
+            foreach (var task in tasks)
+            {
+                extension.Add(task);
+            }
+
+            int taskId = 3;
+            var updatedTask = extension.ChangeStatusById(taskId);
+
+            Assert.That(updatedTask.isCompleted , Is.False);
+        }
     }
 }
