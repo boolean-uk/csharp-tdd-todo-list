dce9052ea92b1c8c2ae0ade4bf7611f905f15ef9
Ateeb <ateeb@live.no>
2025-01-09 15:09:11 +0100

    done

--- tdd-todo-list.CSharp.Main/ToDoList.cs
+++ tdd-todo-list.CSharp.Main/ToDoList.cs
@@ -1,12 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace tdd_todo_list.CSharp.Main
 {
     public class TodoList
     {
+        public Dictionary<string, string> toDoList;
+        public TodoList()
+        {
+            this.toDoList = new Dictionary<string, string>();
+        }
+
+        public bool addTask(string task)
+        {
+            if (toDoList.ContainsKey(task))
+            {
+                return false;
+            }
+            toDoList.Add(task, "incomplete");
+            return true;
+        }
+        public string changeStatus(string task)
+        {
+            if (toDoList.ContainsKey(task))
+            {
+                string currentStatus = toDoList[task];
+                string newStatus = currentStatus.Equals("incomplete") ? "complete" : "incomplete";
+                toDoList[task] = newStatus;
+                return newStatus;
+            }
+            return "Task not found";
+        }
+        public bool seeAllTasks()
+        {
+            Console.WriteLine(toDoList.ToString());
+            return true;
+        }
+        public string GetCompleteTask()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string task in toDoList.Keys)
+            {
+                if ("complete".Equals(toDoList[task]))
+                {
+                    if (sb.Length > 0)
+                    {
+                        sb.Append(", ");
+                    }
+                    sb.Append(task);
+                }
+            }
+            Console.WriteLine(sb.ToString());
+            return sb.ToString();
+        }
+        public string GetIncompleteTask()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string task in toDoList.Keys)
+            {
+                if ("incomplete".Equals(toDoList[task]))
+                {
+                    if (sb.Length > 0)
+                    {
+                        sb.Append(", ");
+                    }
+                    sb.Append(task);
+                }
+            }
+            Console.WriteLine(sb.ToString());
+            return sb.ToString();
+        }
+
+        public string searchTask(string task)
+        {
+            if (toDoList.ContainsKey(task))
+            {
+                return "Task found: " + task;
+            }
+            else
+            {
+                return "Task not found";
+            }
+        }
+        public bool removeTask(string task)
+        {
+            if (toDoList.ContainsKey(task))
+            {
+                toDoList.Remove(task);
+                return true;
+            }
+            return false;
+        }
+        public List<KeyValuePair<string,string>> SortAsc()
+        {
+            return toDoList.OrderBy(x => x.Key).ToList();
+        }
+
+        public List<KeyValuePair<string, string>> SortDesc()
+        {
+            return toDoList.OrderBy(x => x.Key).ToList();
+        }
     }
 }
--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -13,5 +13,82 @@ namespace tdd_todo_list.CSharp.Test
             TodoList core = new TodoList();
             Assert.Pass();
         }
+
+        [Test]
+        public void TestAdd()
+        {
+            TodoList core = new TodoList();
+
+            Assert.IsTrue(core.addTask("run"));
+        }
+        [Test]
+        public void TestSeeTask()
+        {
+            TodoList core = new TodoList();
+
+            //Assert.That(core.seeAllTasks() , Is.EqualTo(Console.ReadLine("")));
+            Assert.IsTrue(core.seeAllTasks());
+        }
+
+        [Test]
+        public void TestRemove()
+        {
+            TodoList core = new TodoList();
+            core.addTask("run");
+            //Assert.That(core.seeAllTasks() , Is.EqualTo(Console.ReadLine("")));
+            Assert.IsTrue(core.removeTask("run"));
+        }
+        [Test]
+        public void TestSearchTask()
+        {
+            TodoList core = new TodoList();
+            core.addTask("run");
+
+            Assert.That(core.searchTask("run") , Is.EqualTo("Task found: run"));
+            
+        }
+
+        [Test]
+        public void TestInCompletedTask()
+        {
+            TodoList core = new TodoList();
+            core.addTask("run");
+            //Assert.That(core.seeAllTasks() , Is.EqualTo(Console.ReadLine("")));
+            Assert.That(core.GetIncompleteTask(), Is.EqualTo("run"));
+         }
+
+        [Test]
+        public void TestCompleteTask()
+        {
+            TodoList core = new TodoList();
+            core.addTask("run");
+            core.changeStatus("run");
+
+            //Assert.That(core.seeAllTasks() , Is.EqualTo(Console.ReadLine("")));
+            Assert.That(core.GetCompleteTask(), Is.EqualTo("run"));
+        }
+
+        [Test]
+        public void TestChangeStatus()
+        {
+            TodoList core = new TodoList();
+            core.addTask("walk");
+            Assert.That(core.changeStatus("walk"), Is.EqualTo("complete"));
+        }
+
+        [Test]
+        public void TestSort()
+        {
+            TodoList core = new TodoList();
+            core.addTask("run");
+            core.addTask("walk");
+            core.addTask("step");
+            core.addTask("sit");
+
+            //Assert.That(core.seeAllTasks() , Is.EqualTo(Console.ReadLine("")));
+            Assert.AreEqual("run", core.SortAsc().FirstOrDefault().Key);
+            Assert.AreEqual("walk", core.SortAsc().LastOrDefault().Key);
+
+        }
     }
 }
