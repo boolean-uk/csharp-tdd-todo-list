d0887aad39e459f24f94fdbffb4fb99611ea1d0e
Erik Bergvall <erik.bergvall12@gmail.com>
2025-01-09 13:27:17 +0100

    Core exersice completed

---
+++ domain-model.md
@@ -0,0 +1,24 @@
+## Core Requirements
+
+- I want to add tasks to my todo list.
+- I want to see all the tasks in my todo list.
+- I want to change the status of a task between incomplete and complete.
+- I want to be able to get only the complete tasks.
+- I want to be able to get only the incomplete tasks.
+- I want to search for a task and receive a message that says it wasn't found if it doesn't exist.
+- I want to remove tasks from my list.
+- I want to see all the tasks in my list ordered alphabetically in ascending order.
+- I want to see all the tasks in my list ordered alphabetically in descending order.
+
+## Domain model
+
+| Classes     | Methods/Properties                  | Senario                                      | Outputs                                               |
+|-------------|-------------------------------------|----------------------------------------------|-------------------------------------------------------|
+| ToDoList.cs | AddTask(string task, bool status) | Adds a task to the list of tasks             | task added                                                      |
+| ToDoList.cs | GetList                         | Get the current list of tasks                | todoList                                              |
+| ToDoList.cs | ChangeStatus(string task)           | Changes the status of a task                 | the new status                                        |
+| ToDoList.cs | GetTasks(bool status)              | Gets the tasks of a specific status          | The tasks with a specific status                      |
+| ToDoList.cs | SearchTask(string input)            | Search for the task that the user has put in | The task that was searched for or a not found message |
+| ToDoList.cs | RemoveTask(string task)             | removes the task from list                   | Removed task                                          |
+| ToDoList.cs | AscSortList()                       | sorts the list in asceending order           | Sorted list                                           |
+| ToDoList.cs | DesSortList()                       | sorts the list in descending order           | Sorted list                                           |
--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -1,5 +1,6 @@
 ﻿using tdd_todo_list.CSharp.Main;
 using NUnit.Framework;
+using System.Runtime.InteropServices;
 
 namespace tdd_todo_list.CSharp.Test
 {
@@ -8,10 +9,106 @@ namespace tdd_todo_list.CSharp.Test
     {
 
         [Test]
-        public void FirstTest()
+        public void AddTest()
+        {
+            Dictionary<string, bool> correctList = new Dictionary<string, bool>();
+            correctList.Add("Make a TodoList", false);
+            TodoList core = new TodoList();
+            core.AddTask("Make a TodoList", false);
+            core.PrintList(core.GetList);
+            Assert.AreEqual(core.GetList, correctList);
+
+        }
+
+        [Test]
+        public void ChangeStatusTest()
+        {
+            Dictionary<string, bool> correctList = new Dictionary<string, bool>();
+            correctList.Add("Make a TodoList", true);
+            TodoList core = new TodoList();
+            core.AddTask("Make a TodoList", false);
+            core.ChangeStatus("Make a TodoList");
+            Assert.AreEqual(core.GetList, correctList);
+        }
+
+        [Test]
+        public void GetTaskStatusTest()
+        {
+            Dictionary<string, bool> correctList = new Dictionary<string, bool>();
+            correctList.Add("Make a AddTest", true);
+            correctList.Add("Make a ChangeStatusTest", true);
+            correctList.Add("Make a SearchTest", true);
+            correctList.Add("Make a GetTaskStatusList", true);
+            correctList.Add("Make a TodoList", true);
+            TodoList core = new TodoList();
+            core.AddTask("Make a AddTest", true);
+            core.AddTask("Make a ChangeStatusTest", true);
+            core.AddTask("Make a SearchTest", true);
+            core.AddTask("Make a GetTaskStatusList", true);
+            core.AddTask("Make a TodoList", true);
+            core.AddTask("Make a Remove Task test", false);
+            core.AddTask("Make a AscSortTest", false);
+            core.AddTask("Make a DescSortTest", false);
+            Assert.AreEqual(core.GetTasks(true), correctList);
+
+        }
+
+        [Test]
+        public void RemoveTest()
+        {
+            Dictionary<string, bool> correctList = new Dictionary<string, bool>();
+            correctList.Add("Make a TodoList", true);
+            correctList.Add("Make a ChangeStatusTest", true);
+            TodoList core = new TodoList();
+            core.AddTask("Make a RemoveTest", false);
+            core.AddTask("Make a ChangeStatusTest", true);
+            core.AddTask("Make a TodoList", true);
+
+            core.RemoveTask("Make a RemoveTest");
+            Assert.AreEqual(core.GetList, correctList);
+        }
+
+        [Test]
+        public void DescSortList()
+        {
+            Dictionary<string, bool> correctList = new Dictionary<string, bool>();
+            correctList.Add("z", true);
+            correctList.Add("a", true);
+
+            TodoList core = new TodoList();
+            core.AddTask("a", true);
+            core.AddTask("z", true);
+            Assert.AreEqual(core.DescSortList(),correctList);
+        }
+
+        [Test]
+        public void AscSortList()
+        {
+            Dictionary<string, bool> correctList = new Dictionary<string, bool>();
+            correctList.Add("a", true);
+            correctList.Add("z", true);
+
+            TodoList core = new TodoList();
+            core.AddTask("z", true);
+            core.AddTask("a", true);
+            Assert.AreEqual(core.AscSortList(), correctList);
+        }
+
+
+
+        [TestCase("Make a TodoList", false)]
+        public void SearchTestFound(string task, bool status)
         {
             TodoList core = new TodoList();
-            Assert.Pass();
+            core.AddTask(task, status);
+            Assert.IsTrue(core.Search(task));
+        }
+
+        [TestCase("Make a TodoList", false)]
+        public void SearchTestNotFound(string task, bool status)
+        {
+            TodoList core = new TodoList();
+            Assert.IsFalse(core.Search(task));
         }
     }
 }
