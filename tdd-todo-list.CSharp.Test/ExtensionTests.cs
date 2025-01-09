4b91b336d86afa76d887b0bb7038da2afc9259cf
GRehan <GRehan@N-NO-01-01-3380>
2025-01-09 14:44:42 +0100

    Extension: + TEST GetDateCreated()

--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -79,5 +79,26 @@ namespace tdd_todo_list.CSharp.Test
 
             Assert.That(updatedTask.isCompleted , Is.False);
         }
+        [Test]
+        public void GetDateCreated()
+        {
+            var task1 = new Task("clean your room", false);
+            var task2 = new Task("do the laundry", false);
+            var task3 = new Task("buy groceries", true);
+            var task4 = new Task("prepare dinner", false);
+            var task5 = new Task("read a book", true);
+            List<Task> tasks = new List<Task> { task1, task2, task3, task4, task5 };
+
+            TodoListExtension extension = new TodoListExtension();
+            foreach (var task in tasks)
+            {
+                extension.Add(task);
+            }
+
+            int taskId = 3;
+            var taskWithDate = extension.GetDateCreated(taskId);
+
+            Assert.That(taskWithDate.createdAt, Is.EqualTo(typeof(DateTime)));
+        }
     }
 }
