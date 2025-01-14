37ee0de9ff33eb37bebf027e1e07f6b425a79556
Miadog7Extra <petter.karstensen@gmail.com>
2025-01-14 12:43:04 +0100

    started extension

--- tdd-todo-list.CSharp.Main/Program.cs
+++ tdd-todo-list.CSharp.Main/Program.cs
@@ -5,12 +5,19 @@ using tdd_todo_list.CSharp.Main;
 
 TodoList toDoList = new TodoList();
 
+TodoListExtension extension = new TodoListExtension();
+
 toDoList.AddTask("Buy food");
 toDoList.AddTask("exercise");
 toDoList.AddTask("auy food");
 toDoList.AddTask("cxercise");
 
+extension.AddTask("check");
+
 var allTasks = toDoList.ShowTasks();
 Console.WriteLine(string.Join("\n", allTasks));
+
+var idTasks = extension.FindTaskByID(1);
+Console.WriteLine(string.Join("\n", idTasks));
 
 
--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace tdd_todo_list.CSharp.Test
 {
+    [TestFixture]
     public class ExtensionTests
     {
         private TodoListExtension _extension;
@@ -14,5 +16,31 @@ namespace tdd_todo_list.CSharp.Test
         {
             _extension = new TodoListExtension();
         }
+        [Test]
+        public void FindByID()
+        {
+            TodoListExtension toDoList = new TodoListExtension();
+            toDoList.AddTask("Buy groceries");
+
+            var taskFound = toDoList.FindTaskByID(1);
+            var taskNotFound = toDoList.FindTaskByID(100);
+
+            Assert.AreEqual("Buy groceries - False", taskFound);
+            Assert.AreEqual("no task with id: 100", taskNotFound);
+        }
+        [Test]
+        public void UpdateName()
+        {
+            TodoListExtension toDoList = new TodoListExtension();
+            toDoList.AddTask("Buy groceries");
+
+            var taskupdated = toDoList.UpdateName();
+            var taskNotFound = toDoList.FindTaskByID(100);
+
+            Assert.AreEqual("Buy update - False", taskupdated);
+            Assert.AreEqual("no task with id: 100", taskNotFound);
+        }
+
+
     }
 }
