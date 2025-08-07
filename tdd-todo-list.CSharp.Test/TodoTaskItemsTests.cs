b292bbfcfd638e3ec249b4eaa8c63d7a9ab760b7
Snorre Aldstedt <snorre.alstedt@experisacademy.no>
2025-08-07 13:10:58 +0200

    Create simple classes to test

---
+++ tdd-todo-list.CSharp.Test/TodoTaskItemsTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tdd_todo_list.CSharp.Main;
+
+namespace tdd_todo_list.CSharp.Test
+{
+    public class TodoTaskItemsTests
+    {
+
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+
+}
