a4577f95159c40b74fbf4ca21fca264dba917ad9
Tein Schoemaker <teintjomme@gmail.com>
2025-08-07 21:20:01 +0200

    Added Extension Tests Till TimeCompleted

--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -1,17 +1,179 @@
-﻿using tdd_todo_list.CSharp.Main;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using tdd_todo_list.CSharp.Main;
 
 namespace tdd_todo_list.CSharp.Test
 {
     [TestFixture]
     public class CoreTests
     {
+        [Test]
+        public void AddedTaskToList()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Test", Priority.High, Category.Work);
+
+            //act
+            var tasks = list.GetAllTasks();
+
+            //assert
+            Assert.That(tasks[0].Name, Is.EqualTo("Test"));
+        }
 
         [Test]
-        public void FirstTest()
+        public void ShowsAllTasksOnList()
         {
-            TodoList core = new TodoList();
-            Assert.Pass();
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Test1", Priority.High, Category.Work);
+            list.AddTask("Test2", Priority.Medium, Category.School);
+            list.AddTask("Test3", Priority.Low, Category.Life);
+
+            //act
+            var allTasks = list.GetAllTasks();
+
+            //assert
+            Assert.That(allTasks.Count, Is.EqualTo(3));
+
+        }
+
+        [Test]
+        public void ChangeTaskStatus()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Test", Priority.High, Category.Work);
+
+            //act
+            var changed = list.ChangeTaskStatus(list[0].id, true);
+
+            //assert
+            Assert.That(changed[0].Status, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void GetOnlyCompletedTasks()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Test1", Priority.High, Category.Work);
+            list.AddTask("Test2", Priority.Medium, Category.School);
+
+            //act
+            list.ChangeTaskStatus(list[1].id, true);
+            var completedTasks = list.GetCompletedTasks();
+
+            //assert
+            Assert.That(completedTasks[0].name, Is.EqualTo("Test2"));
+        }
+
+        [Test]
+        public void GetOnlyInCompletedTasks()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Test1", Priority.High, Category.Work);
+            list.AddTask("Test2", Priority.Medium, Category.School);
+
+            //act
+            list.ChangeTaskStatus(list[1].id, false);
+            var completedTasks = list.GetInCompletedTasks();
+
+            //assert
+            Assert.That(completedTasks[0].name, Is.EqualTo("Test2"));
+        }
+
+        [Test]
+        public void ErrorMessageIfTaskNotFound()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Test1", Priority.High, Category.Work);
+
+            //act
+            var search = list.SearchTask("Test32");
+
+            //assert
+            Assert.Null(search);
+        }
+
+        [Test]
+        public void RemoveTaskById()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Test1", Priority.High, Category.Work);
+
+            //act
+            list.RemoveTask(1);
+
+            //assert
+            Assert.Null(list);
+        }
+
+        [Test]
+        public void GetTasksAscendingAZ()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Bread", Priority.High, Category.Work);
+            list.AddTask("Ants", Priority.High, Category.Work);
+            list.AddTask("Crab", Priority.High, Category.Work);
+
+            //act
+            var ascendingList = list.GetTasksAscendingAZ();
+
+            //assert
+            Assert.That(ascendingList[0].name, Is.EqualTo("Ants"));
+            Assert.That(ascendingList[1].name, Is.EqualTo("Bread"));
+            Assert.That(ascendingList[2].name, Is.EqualTo("Crab"));
+        }
+
+        [Test]
+        public void GetTasksDescendingAZ()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Bread", Priority.High, Category.Work);
+            list.AddTask("Ants", Priority.High, Category.Work);
+            list.AddTask("Crab", Priority.High, Category.Work);
+
+            //act
+            var ascendingList = list.GetTasksAscendingAZ();
+
+            //assert
+            Assert.That(ascendingList[2].name, Is.EqualTo("Ants"));
+            Assert.That(ascendingList[1].name, Is.EqualTo("Bread"));
+            Assert.That(ascendingList[0].name, Is.EqualTo("Crab"));
+        }
+
+        [Test]
+        public void ListTasksByPriority()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Test1", Priority.Medium, Category.Work);
+            list.AddTask("Test2", Priority.Low, Category.School);
+            list.AddTask("Test3", Priority.High, Category.Life);
+
+            //act
+            var byPriority = list.GetTasksByPriority();
+
+            //assert
+            Assert.That(byPriority[0].name, Is.EqualTo("Test3"));
+            Assert.That(byPriority[1].name, Is.EqualTo("Test1"));
+            Assert.That(byPriority[2].name, Is.EqualTo("Test2"));
         }
     }
 }
--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -1,18 +1,72 @@
-﻿using tdd_todo_list.CSharp.Main;
+﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tdd_todo_list.CSharp.Main;
 
 namespace tdd_todo_list.CSharp.Test
 {
     public class ExtensionTests
     {
-        private TodoListExtension _extension;
-        public ExtensionTests()
+        [Test]
+        public void FindATaskById()
         {
-            _extension = new TodoListExtension();
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Test", Priority.High, Category.Work);
+
+            //act
+            var tasks = list.GetTaskById(list.id);
+
+            //assert
+            Assert.That(tasks.Name, Is.EqualTo("Test"));
+        }
+
+        [Test]
+        public void ChangeTaskNameById()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+
+            list.AddTask("Test", Priority.High, Category.Work);
+
+            //act
+            list.UpdateTaskName(list.id, "UpdatedTest");
+
+            //assert
+            Assert.That(tasks.Name, Is.EqualTo("UpdatedTest"));
+        }
+
+        [Test]
+        public void TimeCreatedAtIsSet()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+            var currentTime = DateTime.Now;
+            list.AddTask("Test", Priority.High, Category.Work);
+
+            //act
+            var createdAt = list.CreatedAt;
+
+            //assert
+            Assert.That(currentTime, Is.EqualTo(createdAt));
+        }
+
+        public void TimeCompletedAtIsSet()
+        {
+            //arrange
+            ToDoList list = new ToDoList();
+            list.AddTask("Test", Priority.High, Category.Work);
+
+            //act
+            list.ChangeStatusById(list.id, true);
+            var completedAt = list.CompletedAt;
+
+            //assert
+            Assert.That(completedAt, Is.EqualTo(null!));
         }
     }
 }
