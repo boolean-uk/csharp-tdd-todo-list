0e1559f5d5886073b6d841617e0b0b2ab63efceb
Patrick Schuur <patrickschuurdev@gmail.com>
2025-08-19 16:24:20 +0200

    Declared functions

---
+++ tdd-todo-list.CSharp.Main/ExtendedTask.cs
@@ -0,0 +1,33 @@
+﻿namespace tdd_todo_list.CSharp.Main;
+
+public class ExtendedTask
+{
+    public readonly Guid Id;
+    public readonly DateTime CreateTime;
+    public TaskStatus Status { get; private set; }
+    public DateTime? CompleteTime { get; private set; }
+    
+    public string Name;
+    public string Description;
+
+    public ExtendedTask(string name, string description)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Description = description;
+        Status = TaskStatus.Incomplete;
+        CreateTime = DateTime.Now;
+    }
+
+    public void Complete()
+    {
+        if (Status == TaskStatus.Complete) return;
+        Status = TaskStatus.Complete;
+        CompleteTime = DateTime.Now;
+    }
+
+    public bool Equals(Task other)
+    {
+        return Name == other.Name;
+    }
+}
--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -8,5 +8,56 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoListExtension
     {
+        private List<ExtendedTask> _tasks = new List<ExtendedTask>();
+        
+        public ExtendedTask GetTask(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTaskName(Guid id, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTaskStatus(Guid id, TaskStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetTaskTimeOfCreation(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime? GetTaskTimeOfCompletion(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ExtendedTask FindLongestCompletedTask(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ExtendedTask FindShortestCompletedTask(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ExtendedTask> FindTasksCompletedInMoreThan5Days()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoriseTask(Guid id, TaskCategory category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ExtendedTask> ListTasksByCategory(TaskCategory category)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
